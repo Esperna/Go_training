90f9d9b5d6f66b3988e800979e38ae4515b9d57f
esper0328 <esper0328@gmail.com>
2016-03-13 13:52:51 +0900

    Ex3_4 no_shift

--- ch3/3_4/main.go
+++ ch3/3_4/main.go
@@ -11,21 +11,47 @@ import (
 	"fmt"
 	"log"
 	"math"
+	"math/rand"
 	"net/http"
+	"time"
+	"os"
+	"strconv"
+	"strings"
 )
 
+var width,height  = 600,320		            // canvas size in pixels
+var	xyrange       = 30                // axis ranges (-xyrange..+xyrange)
+var	xyscale = width / 2 / xyrange // pixels per x or y unit
+var	zscale 		  = float64(height) * float64(0.4)        // pixels per z unit
+var color = "grey"
 const (
-	width, height = 600, 320            // canvas size in pixels
 	cells         = 100                 // number of grid cells
-	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
-	xyscale       = width / 2 / xyrange // pixels per x or y unit
-	zscale        = height * 0.4        // pixels per z unit
 	angle         = math.Pi / 6         // angle of x, y axes (=30ﾂｰ)
 )
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30ﾂｰ), cos(30ﾂｰ)
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			r.ParseForm()
+			for k, v := range r.Form {
+				if k == "height" {
+					height, _ = strconv.Atoi(strings.Join(v, ""))
+				}else if k == "width" {
+					width, _ = strconv.Atoi(strings.Join(v, ""))
+				}else if k == "color"{
+					color = v[0]
+				}
+			}
+			drawSVG(w, r)
+		}
+		http.HandleFunc("/", handler)
+		//!-http
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+		return
+	}
 	http.HandleFunc("/draw", drawSVG)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
@@ -33,8 +59,8 @@ func main() {
 func drawSVG(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "image/svg+xml")
 	fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+
-		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
-		"width='%d' height='%d'>", width, height)
+		"style='stroke: %s; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>",color, width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
 			ax, ay := corner(i+1, j)
@@ -50,15 +76,15 @@ func drawSVG(w http.ResponseWriter, r *http.Request) {
 
 func corner(i, j int) (float64, float64) {
 	// Find point (x,y) at corner of cell (i,j).
-	x := xyrange * (float64(i)/cells - 0.5)
-	y := xyrange * (float64(j)/cells - 0.5)
+	x := float64(xyrange) * (float64(i)/cells - 0.5)
+	y := float64(xyrange) * (float64(j)/cells - 0.5)
 
 	// Compute surface height z.
 	z := f(x, y)
 
 	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
-	sx := width/2 + (x-y)*cos30*xyscale
-	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	sx := float64(width/2) + float64((x-y))*cos30*float64(xyscale)
+	sy := float64(height/2) + float64((x+y))*sin30*float64(xyscale) - z*zscale
 	return sx, sy
 }
 

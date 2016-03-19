a0017f4b465fb02aadc8cca7df02864c8917f2fd
esper0328 <esper0328@gmail.com>
2016-03-19 17:46:01 +0900

    3_11

---
+++ ch3/3_11/main.go
@@ -0,0 +1,75 @@
+// Copyright ﾂｩ 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 73.
+
+// Comma prints its argument numbers with a comma at each power of 1000.
+//
+// Example:
+// 	$ go build gopl.io/ch3/comma
+//	$ ./comma 1 12 123 1234 1234567890
+// 	1
+// 	12
+// 	123
+// 	1,234
+// 	1,234,567,890
+//
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	for i := 1; i < len(os.Args); i++ {
+		fmt.Printf("  %s\n", comma(os.Args[i]))
+	}
+}
+
+//!+
+// comma inserts commas in a non-negative decimal integer string.
+func comma(s string) string {
+	var sign, integer, decimal string
+
+	if strings.HasPrefix(s, "+") {
+		sign = "+"
+		s = s[1:]
+	} else if strings.HasPrefix(s, "-") {
+		sign = "-"
+		s = s[1:]
+	} else {
+		sign = "+"
+	}
+
+	if strings.Contains(s, ".") {
+		for i := len(s) - 1; i >= 0; i-- {
+			if s[i] == '.' {
+				integer = s[:i]
+				decimal = s[i:]
+				break
+			}
+		}
+	} else {
+		integer = s
+		decimal = ""
+	}
+
+	n := len(integer)
+	if n <= 3 {
+		return sign + integer + decimal
+	}
+
+	var buf bytes.Buffer
+	for i := 0; i < n; i++ {
+		if (i > 0) && ((((n - 3) - i) % 3) == 0) {
+			buf.WriteString(",")
+		}
+		buf.WriteByte(integer[i])
+	}
+	integer = buf.String()
+	return sign + integer + decimal
+
+}

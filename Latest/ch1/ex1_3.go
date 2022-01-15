af47332a6feea114f4ae4a84bcc95ae59b1cdb2a
Esper0328 <yoheikato@katouyouheinoMacBook-Air.local>
2022-01-15 20:28:17 +0900

    display exec time of with/without strings.Join

---
+++ Latest/ch1/ex1_3.go
@@ -0,0 +1,34 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 4.
+//!+
+
+// Echo1 prints its command-line arguments.
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+	secs := time.Since(start).Seconds()
+	fmt.Println(secs)
+
+	start = time.Now()
+	fmt.Println(strings.Join(os.Args[1:], " "))
+	secs = time.Since(start).Seconds()
+	fmt.Println(secs)
+}
+
+//!-

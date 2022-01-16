f83ef9c30bd5c508de7accd102ce9dce3943a774
Esper0328 <yoheikato@katouyouheinoMacBook-Air.local>
2022-01-16 23:32:12 +0900

    displayed filenames including duplicated line among multiple files

--- Latest/ch1/ex1_4.go
+++ Latest/ch1/ex1_4.go
@@ -17,8 +17,9 @@ import (
 func main() {
 	counts := make(map[string]int)
 	files := os.Args[1:]
+	var duplines []string
 	if len(files) == 0 {
-		//		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -33,14 +34,40 @@ func main() {
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
+			duplines = append(duplines, line)
 		}
 	}
+
+	for _, arg := range files {
+		f, err := os.Open(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+			continue
+		}
+		dispFilenamesIncludingDuplicatedLines(f, duplines, arg)
+		f.Close()
+	}
+
 }
 
 func countLines(f *os.File, counts map[string]int) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+}
+
+func dispFilenamesIncludingDuplicatedLines(f *os.File, duplines []string, filename string) {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		for _, line := range duplines {
+			if input.Text() == line && counts[line] == 0 {
+				fmt.Printf("%s\t%s\n", line, filename)
+				counts[line]++
+			}
+		}
 	}
 	// NOTE: ignoring potential errors from input.Err()
 }

9b5982eea53614894a2aaccc914eecfaf5e1141d
Esper0328 <yoheikato@katouyouheinoMacBook-Air.local>
2022-01-17 15:16:10 +0900

    changed color index name

--- Latest/ch1/ex1_5.go
+++ Latest/ch1/ex1_5.go
@@ -32,8 +32,8 @@ import (
 var palette = []color.Color{color.Black, color.RGBA{0x53, 0xBF, 0x49, 0xff}}
 
 const (
-	whiteIndex = 0 // first color in palette
-	blackIndex = 1 // next color in palette
+	backgroundwhiteIndex = 0 // first color in palette
+	foregroundIndex      = 1 // next color in palette
 )
 
 func main() {
@@ -75,7 +75,7 @@ func lissajous(out io.Writer) {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				blackIndex)
+				foregroundIndex)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)

c9f3b71eca08bd754eeeefe31c85525d89802f20
Esper0328 <yoheikato@katouyouheinoMacBook-Air.local>
2022-01-23 22:10:08 +0900

    recommited because ex2_3 folder was long an ex2_4 is true folder

---
+++ ch2/ex2_4/popcount/go.mod
@@ -0,0 +1,3 @@
+module ch2/ex2_4/popcount
+
+go 1.17
---
+++ ch2/ex2_4/popcount/main.go
@@ -0,0 +1,48 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 45.
+
+// (Package doc comment intentionally malformed to demonstrate golint.)
+//!+
+package popcount
+
+// pc[i] is the population count of i.
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCount returns the population count (number of set bits) of x.
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func PopCountByLoop(x uint64) int {
+	var sum int
+	for i := 0; i < 8; i++ {
+		sum += int(pc[byte(x>>(i*8))])
+	}
+	return sum
+}
+
+func PopCountByShift(x uint64) int {
+	count := 0
+	for i := 0; i < 64; i++ {
+		count += int(x & 1)
+		x = x >> 1
+	}
+	return count
+}
+
+//!-

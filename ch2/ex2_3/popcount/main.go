6153bbdff19e6423963f93bd8d4854d2a8d0c212
Esper0328 <yoheikato@katouyouheinoMacBook-Air.local>
2022-01-23 21:08:34 +0900

    used LSB and bit shift

--- ch2/ex2_3/popcount/main.go
+++ ch2/ex2_3/popcount/main.go
@@ -36,4 +36,13 @@ func PopCountByLoop(x uint64) int {
 	return sum
 }
 
+func PopCountByShift(x uint64) int {
+	count := 0
+	for i := 0; i < 64; i++ {
+		count += int(x & 1)
+		x = x >> 1
+	}
+	return count
+}
+
 //!-
--- ch2/ex2_3/popcount/popcount_test.go
+++ ch2/ex2_3/popcount/popcount_test.go
@@ -6,7 +6,6 @@ package popcount_test
 import (
 	"ch2/ex2_3/popcount"
 	"testing"
-	//	"gopl.io/ch2/popcount"
 )
 
 // -- Alternative implementations --
@@ -47,6 +46,13 @@ func BenchmarkPopCount(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		popcount.PopCount(0x1234567890ABCDEF)
 	}
+}
+
+func BenchmarkPopCountByShift(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		popcount.PopCountByShift(0x1234567890ABCDEF)
+	}
+
 }
 
 func BenchmarkPopCountByLoop(b *testing.B) {

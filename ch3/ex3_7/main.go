75523a58bc59f2d61df52726de87a2615cf0eb01
Esper0328 <yoheikato@katouyouheinoMacBook-Air.local>
2022-01-26 10:20:51 +0900

    added width, height for image.Rect

--- ch3/ex3_7/main.go
+++ ch3/ex3_7/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
-	"image/color"
 	"math/cmplx"
 )
 
 func main() {
-	var buf = complex(100, 100)
+	const (
+		width, height = 1024, 1024
+	)
+	var buf = complex(width, height)
 	var z0, z1 complex128
 	var count int
 	for {
@@ -16,7 +18,7 @@ func main() {
 		fmt.Println(z1)
 		buf = z1
 		count++
-		img.Set(real(z1), imag(z1), color.Black)
+		//img.Set(real(z1), imag(z1), color.Black)
 		if cmplx.Abs(z1-z0) < 0.001 {
 			break
 		}

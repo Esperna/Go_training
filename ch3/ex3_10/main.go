3bda1baf353c1e15f3212fac9c6d45e0b7f0bb10
Esper0328 <yoheikato@katouyouheinoMacBook-Air.local>
2022-01-28 22:49:21 +0900

    without recusive comma but not usebytes.Buffer

--- ch3/ex3_10/main.go
+++ ch3/ex3_10/main.go
@@ -1,7 +1,47 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 73.
+
+// Comma prints its argument numbers with a comma at each power of 1000.
+//
+// Example:
+// 	$ go build gopl.io/ch3/comma
+//	$ ./comma 1 12 123 1234 1234567890
+// 	1
+// 	12
+// 	123
+// 	1,234
+// 	1,234,567,890
+//
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	for i := 1; i < len(os.Args); i++ {
+		fmt.Printf("  %s\n", comma(os.Args[i]))
+	}
 }
+
+//!+
+// comma inserts commas in a non-negative decimal integer string.
+func comma(s string) string {
+	n := len(s)
+	i := n % 3
+	sum := s[:i]
+	for n >= 3 {
+		if i > 0 {
+			sum += ","
+		}
+		sum += s[i : i+3]
+		i += 3
+		n -= 3
+	}
+	return sum
+}
+
+//!-

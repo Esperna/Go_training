3e6dd6f5816b19e308a13f756da8a3339b4e3a92
Esper0328 <esper0328@gmail.com>
2022-01-29 14:58:48 +0900

    sorted and then compared

--- ch3/ex3_12/main.go
+++ ch3/ex3_12/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"sort"
+)
+
+type ByRune []rune
+
+func (r ByRune) Len() int { return len(r) }
+
+func (r ByRune) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
+func (r ByRune) Less(i, j int) bool { return r[i] < r[j] }
+
+func StringToRuneSlice(s string) []rune {
+	var r []rune
+	for _, runeValue := range s {
+		r = append(r, runeValue)
+	}
+	return r
+}
+
+func SortStringByCharacter(s string) string {
+	var r ByRune = StringToRuneSlice(s)
+	sort.Sort(r)
+	return string(r)
+}
 
 func main() {
-	fmt.Println("vim-go")
+	if len(os.Args) == 3 {
+		if SortStringByCharacter(os.Args[1]) == SortStringByCharacter(os.Args[2]) {
+			fmt.Println("Anagram!")
+		} else {
+			fmt.Println("Not Anagram!")
+		}
+	} else {
+		fmt.Println("Invalid Number of Argument")
+	}
 }

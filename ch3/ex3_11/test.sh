259e1b65f3549aed9cefbd861cdf4bad198cf853
Esper0328 <yoheikato@katouyouheinoMacBook-Air.local>
2022-01-29 12:17:19 +0900

    displayed Input->Expected

--- ch3/ex3_11/test.sh
+++ ch3/ex3_11/test.sh
@@ -1,20 +1,20 @@
 #!/bin/zsh
 
 go build ./main.go
-echo "Expected:"
-echo 1
-echo 1.2
-echo 123
-echo 1,234
-echo 12,345
-echo 12,345.
-echo 123,456
-echo 123,456.789
-echo 1,234,567
-echo 1,234,567.8
-echo 1,234,567.89
-echo +1,234,567.89
-echo -123.456789
+echo "Input->Expected:"
+echo 1              -> 1
+echo 1.2            -> 1.2
+echo 123            -> 123
+echo 1234           -> 1,234
+echo 12345          -> 12,345
+echo 12345.         -> 12,345.
+echo 123456         -> 123,456
+echo 123456.789     -> 123,456.789
+echo 1234567        -> 1,234,567
+echo 1234567.8      -> 1,234,567.8
+echo 1234567.89     -> 1,234,567.89
+echo +1234567.89    -> +1,234,567.89
+echo -123.456789    -> -123.456789
 
 echo "Actual:"
 ./main 1

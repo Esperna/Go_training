91ac360822c7856e68b1288859ba2229503a0825
Esper0328 <esper0328@gmail.com>
2022-01-29 15:02:27 +0900

    replaced ByRune with RuneSlice

--- ch3/ex3_12/main.go
+++ ch3/ex3_12/main.go
@@ -6,12 +6,12 @@ import (
 	"sort"
 )
 
-type ByRune []rune
+type RuneSlice []rune
 
-func (r ByRune) Len() int { return len(r) }
+func (r RuneSlice) Len() int { return len(r) }
 
-func (r ByRune) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
-func (r ByRune) Less(i, j int) bool { return r[i] < r[j] }
+func (r RuneSlice) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
+func (r RuneSlice) Less(i, j int) bool { return r[i] < r[j] }
 
 func StringToRuneSlice(s string) []rune {
 	var r []rune
@@ -22,7 +22,7 @@ func StringToRuneSlice(s string) []rune {
 }
 
 func SortStringByCharacter(s string) string {
-	var r ByRune = StringToRuneSlice(s)
+	var r RuneSlice = StringToRuneSlice(s)
 	sort.Sort(r)
 	return string(r)
 }

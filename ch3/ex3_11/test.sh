17efe6331521e8287310629884b29023d02d2f3e
Esper0328 <yoheikato@katouyouheinoMacBook-Air.local>
2022-01-29 11:05:08 +0900

    displayed Expected and Acutual

--- ch3/ex3_11/test.sh
+++ ch3/ex3_11/test.sh
@@ -1,6 +1,22 @@
 #!/bin/zsh
 
 go build ./main.go
+echo "Expected:"
+echo 1
+echo 1.2
+echo 123
+echo 1234
+echo 12345
+echo 12345.
+echo 123456
+echo 123456.789
+echo 1234567
+echo 1234567.8
+echo 1234567.89
+echo +1234567.89
+echo -123.456789
+
+echo "Actual:"
 ./main 1
 ./main 1.2
 ./main 123

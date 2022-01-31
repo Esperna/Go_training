b1b5508c82ec82d880c628295ab50edaca1f447b
Esper0328 <esper0328@gmail.com>
2022-01-31 21:42:51 +0900

    count different bit between x and X hush

--- ch4/ex4_1/main.go
+++ ch4/ex4_1/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"ch4/ex4_1/popcount"
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	c1 := sha256.Sum256([]byte("x"))
+	c2 := sha256.Sum256([]byte("X"))
+	count := 0
+	for i := 0; i < 4; i++ {
+		c1_i := binary.LittleEndian.Uint64(c1[8*i : 8*(i+1)])
+		c2_i := binary.LittleEndian.Uint64(c2[8*i : 8*(i+1)])
+		c3_i := c1_i ^ c2_i
+		count += popcount.PopCount(c3_i)
+		fmt.Println(count)
+	}
 }

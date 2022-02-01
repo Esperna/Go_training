8d7ebb472c0c3546d8912b16c894e0468ba9dfb8
Esper0328 <esper0328@gmail.com>
2022-02-01 13:44:06 +0900

    array pointer version of reverse

--- ch4/ex4_3/main.go
+++ ch4/ex4_3/main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("vim-go")
+	a := [6]int{0, 1, 2, 3, 4, 5}
+	reverse(&a)
+	fmt.Println(a)
+}
+
+func reverse(ptr *[6]int) {
+	for i := 0; i < 5-i; i++ {
+		tmp := ptr[i]
+		ptr[i] = ptr[5-i]
+		ptr[5-i] = tmp
+	}
 }

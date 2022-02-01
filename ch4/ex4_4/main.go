6ce83d10a895c5a8a0c03726713465c3f45d44f5
Esper0328 <esper0328@gmail.com>
2022-02-01 14:40:53 +0900

    modified error of last argument -r

--- ch4/ex4_4/main.go
+++ ch4/ex4_4/main.go
@@ -1,11 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	x := []int{0, 1, 2, 3, 4, 5}
-	x = rotate(x, 2)
-	fmt.Println(x)
+	var x []int
+	length := len(os.Args)
+	if length > 4 {
+		for i := 1; i < length; i++ {
+			if os.Args[i] != "-r" {
+				j, err := strconv.Atoi(os.Args[i])
+				if err != nil {
+					fmt.Println("Invalid Argument.")
+					return
+				}
+				x = append(x, j)
+			} else {
+				if i+1 < length {
+					r, err := strconv.Atoi(os.Args[i+1])
+					if err != nil {
+						fmt.Println("Invalid Argument.")
+						return
+					}
+					x = rotate(x, r)
+					fmt.Println(x)
+				} else {
+					fmt.Println("Invalid Argument.")
+				}
+			}
+		}
+	} else {
+		fmt.Println("Invalid Number of Argument. More than 3(value1 value2 -r rotateValue) is expected.")
+	}
 }
 
 func rotate(s []int, i int) []int {

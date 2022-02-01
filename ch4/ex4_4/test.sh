67c5e8798b48cb0051cbc4ae99d250d574362d45
Esper0328 <esper0328@gmail.com>
2022-02-01 14:36:06 +0900

    added test

---
+++ ch4/ex4_4/test.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+go build ./main.go
+echo "./main 1"
+./main 1
+echo "./main 1 2 3"
+./main 1 2 3
+echo "./main 1 2 3 -r 1"
+./main 1 2 3 -r 1
+echo "./main 1 2 3 4 5 6 7 8 -r 5"
+./main 1 2 3 4 5 6 7 8 -r 5
+echo ./main 1 2 3 -r
+./main 1 2 3 -r

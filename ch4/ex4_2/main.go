325d40e396e1567f3e17622e9fc0e7334ce96123
Esper0328 <esper0328@gmail.com>
2022-02-01 11:15:00 +0900

    implemented displaying SHA256,384,512

--- ch4/ex4_2/main.go
+++ ch4/ex4_2/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"crypto/sha512"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	length := len(os.Args)
+	if length == 2 {
+		c := sha256.Sum256([]byte(os.Args[1]))
+		fmt.Printf("%x\n", c)
+	} else if length == 3 {
+		if os.Args[2] == "-256" {
+			c := sha256.Sum256([]byte(os.Args[1]))
+			fmt.Printf("%x\n", c)
+		} else if os.Args[2] == "-384" {
+			c := sha512.Sum384([]byte(os.Args[1]))
+			fmt.Printf("%x\n", c)
+		} else if os.Args[2] == "-512" {
+			c := sha512.Sum512([]byte(os.Args[1]))
+			fmt.Printf("%x\n", c)
+
+		} else {
+			fmt.Printf("Invalid Option. -256,-384,or -512 is Expected\n")
+		}
+	} else {
+		fmt.Printf("Invalid Number of Argument. Expected Number of Argument is 2 or 3.\n")
+		fmt.Printf("Ex: ./main abc -256\n")
+	}
 }

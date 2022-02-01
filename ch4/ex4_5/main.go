42dfebbc65ebf2a0f233c7bb6a2b09d4a1c008cf
Esper0328 <esper0328@gmail.com>
2022-02-01 15:48:11 +0900

    solved some pattern of neigbor duplicated slice

--- ch4/ex4_5/main.go
+++ ch4/ex4_5/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	length := len(os.Args)
+	if length == 2 {
+		s := os.Args[1]
+		b := []byte(s)
+		fmt.Println(s)
+		b = deleteDup(b)
+		fmt.Println(string(b))
+	} else {
+		fmt.Println("Invalid Number of Argument")
+	}
+}
+
+func deleteDup(s []byte) []byte {
+	for i := 0; i < len(s); i++ {
+		t := s[i]
+		for j := i + 1; j < len(s); j++ {
+			if s[j] == t {
+				s = remove(s, j)
+				s = deleteDup(s)
+			}
+		}
+	}
+	return s
+}
+
+func remove(slice []byte, i int) []byte {
+	copy(slice[i:], slice[i+1:])
+	return slice[:len(slice)-1]
 }

a85ac00878fd9c150ce0e15b4a8b11b92400e002
Esper0328 <esper0328@gmail.com>
2022-02-01 14:07:39 +0900

    used slice append

--- ch4/ex4_4/main.go
+++ ch4/ex4_4/main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("vim-go")
+	x := []int{0, 1, 2, 3, 4, 5}
+	x = rotate(x, 2)
+	fmt.Println(x)
+}
+
+func rotate(s []int, i int) []int {
+	return append(s[i:len(s)], s[0:i]...)
 }

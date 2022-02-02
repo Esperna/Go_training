0bf24ec8e94402fb14e631316710bc85fad1c5e1
Esper0328 <esper0328@gmail.com>
2022-02-03 08:54:16 +0900

    initial commit for practice

--- ch4/ex4_9/main.go
+++ ch4/ex4_9/main.go
@@ -14,20 +14,24 @@ import (
 
 //!+
 func main() {
-	seen := make(map[string]bool) // a set of strings
+	seen := make(map[string]int) // a set of strings
 	input := bufio.NewScanner(os.Stdin)
 	input.Split(bufio.ScanWords)
 	for input.Scan() {
 		line := input.Text()
-		if !seen[line] {
-			seen[line] = true
-			fmt.Println(line)
-		}
+		seen[line]++
 	}
 
 	if err := input.Err(); err != nil {
 		fmt.Fprintf(os.Stderr, "dedup: %v\n", err)
 		os.Exit(1)
+	}
+	var names []string
+	for w, _ := range seen {
+		names = append(names, w)
+	}
+	for _, name := range names {
+		fmt.Printf("%s\t%d\n", name, seen[name])
 	}
 }
 
---
+++ ch4/ex4_a/go.mod
@@ -0,0 +1,3 @@
+module ch4/ex4_a
+
+go 1.17

2f49c57bf1eb891f8176da08c1386e942d7e0de3
Esper0328 <esper0328@gmail.com>
2022-02-03 10:05:11 +0900

    Behavior check of treesort

--- ch4/ex4_a/main.go
+++ ch4/ex4_a/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"ch4/ex4_a/treesort"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	length := len(os.Args)
+	if length > 1 {
+		var slice []int
+		for i := 1; i < length; i++ {
+			value, err := strconv.Atoi(os.Args[i])
+			if err != nil {
+				fmt.Println("Invalid String")
+				return
+			}
+			slice = append(slice, value)
+		}
+		treesort.Sort(slice)
+		for _, v := range slice {
+			fmt.Println(v)
+		}
+	} else {
+		fmt.Println("Invalid Number of Argument")
+	}
+
 }

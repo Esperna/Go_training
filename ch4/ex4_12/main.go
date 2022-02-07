8ab8686eb1790ae6df1a0ec8f5b7a69cb48813e3
Esper0328 <esper0328@gmail.com>
2022-02-07 15:55:57 +0900

    displayed offline index but error for 404

--- ch4/ex4_12/main.go
+++ ch4/ex4_12/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+type Comic struct {
+	Month      string
+	Num        int
+	Link       string
+	Year       string
+	News       string
+	SafeTitle  string `json:safe_title`
+	Transcript string
+	Alt        string
+	Img        string
+	Title      string
+	Day        string
+}
 
 func main() {
-	fmt.Println("vim-go")
+	end := 600
+	for i := 1; i < end; i++ {
+		resp, err := http.Get("https://xkcd.com/" + strconv.Itoa(i) + "/info.0.json")
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		var comic Comic
+		if err := json.Unmarshal(b, &comic); err != nil {
+			fmt.Printf("%v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%d\t%s/%s\t%s\n", comic.Num, comic.Month, comic.Year, comic.Title)
+	}
 }

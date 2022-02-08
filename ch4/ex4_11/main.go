12c9b0cbb27b387925c6592742dd2b239baaf33d
Esper0328 <esper0328@gmail.com>
2022-02-08 15:41:24 +0900

    created issue with arguments

--- ch4/ex4_11/main.go
+++ ch4/ex4_11/main.go
@@ -4,23 +4,20 @@ import (
 	"ch4/ex4_11/github"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	length := len(os.Args)
-	if length == 3 {
-		/*	issue1 := new(github.Issue)
-			issue1.Title = "Additional Issue"
-			issue1.Body = "This shall be solved"
-
-			issue1_json, _ := json.Marshal(issue1)
-			fmt.Printf("%s\n", string(issue1_json))
-			github.CreateIssue(bytes.NewBuffer(issue1_json))
-		*/
-		params := `{"title": "test2"}`
-		github.CreateIssue(os.Args[1], os.Args[2], params)
+	if length == 6 {
+		json_str := "{" + strconv.Quote("title") + ":" + strconv.Quote(os.Args[3])
+		json_str += "," + strconv.Quote("body") + ":" + strconv.Quote(os.Args[4])
+		json_str += "," + strconv.Quote("labels") + ":" + "[" + strconv.Quote(os.Args[5]) + "]"
+		json_str += "}"
+		fmt.Printf("%s\n", json_str)
+		github.CreateIssue(os.Args[1], os.Args[2], json_str)
 		github.ReadIssues()
 	} else {
-		fmt.Println("Invalid Number of Argument. ./main ID Token is expected")
+		fmt.Println("Invalid Number of Argument. ./main ID Token Title Body Labels is expected")
 	}
 }

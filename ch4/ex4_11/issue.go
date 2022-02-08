89a16b5c40c290ed195e3b1bd8f169f67588db10
Esper0328 <esper0328@gmail.com>
2022-02-08 18:27:09 +0900

    Added option to command line argumens

--- ch4/ex4_11/issue.go
+++ ch4/ex4_11/issue.go
@@ -9,15 +9,29 @@ import (
 
 func main() {
 	length := len(os.Args)
-	if length == 6 {
-		json_str := "{" + strconv.Quote("title") + ":" + strconv.Quote(os.Args[3])
-		json_str += "," + strconv.Quote("body") + ":" + strconv.Quote(os.Args[4])
-		json_str += "," + strconv.Quote("labels") + ":" + "[" + strconv.Quote(os.Args[5]) + "]"
-		json_str += "}"
-		fmt.Printf("%s\n", json_str)
-		github.CreateIssue(os.Args[1], os.Args[2], json_str)
-		github.ReadIssues()
+	if length < 1 || length > 7 {
+		fmt.Println("Invalid Number of Argument. ./issue -option(-c,-r,-u, or -d) GitHubID Token Title Body Labels is expected")
+		os.Exit(1)
 	} else {
-		fmt.Println("Invalid Number of Argument. ./main ID Token Title Body Labels is expected")
+		if os.Args[1] == "-c" {
+			if length == 7 {
+				json_str := "{" + strconv.Quote("title") + ":" + strconv.Quote(os.Args[4])
+				json_str += "," + strconv.Quote("body") + ":" + strconv.Quote(os.Args[5])
+				json_str += "," + strconv.Quote("labels") + ":" + "[" + strconv.Quote(os.Args[6]) + "]"
+				json_str += "}"
+				fmt.Printf("%s\n", json_str)
+				github.CreateIssue(os.Args[2], os.Args[3], json_str)
+				github.ReadIssues()
+			}
+		} else if os.Args[1] == "-r" {
+			github.ReadIssues()
+		} else if os.Args[1] == "-u" {
+			fmt.Printf("TBD\n")
+		} else if os.Args[1] == "-d" {
+			fmt.Printf("TBD\n")
+		} else {
+			fmt.Println("Invalid Option. ./issue -option(-c,-r,-u, or -d) GitHubID Token Title Body Labels is expected")
+			os.Exit(1)
+		}
 	}
 }

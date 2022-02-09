ee55e0baff5db2b80636688eaad8f219bdcb3655
Esper0328 <esper0328@gmail.com>
2022-02-09 17:03:42 +0900

    modified print display

--- ch4/ex4_14/issue.go
+++ ch4/ex4_14/issue.go
@@ -23,7 +23,7 @@ func main() {
 	}
 	fmt.Printf("%d issues:\n", result.TotalCount)
 
-	fmt.Println("\n<Milestone List>")
+	fmt.Println("\n<Bug List>")
 	milestones := make(map[string]string)
 	for _, item := range result.Items {
 		fmt.Printf("#%-5d %9.9s %.55s\n",

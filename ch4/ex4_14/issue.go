27f2de2eddf00e14c40ed5603292aded0f860e26
Esper0328 <esper0328@gmail.com>
2022-02-09 21:38:47 +0900

    applied html template partially

--- ch4/ex4_14/issue.go
+++ ch4/ex4_14/issue.go
@@ -9,11 +9,32 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 
 	"ch4/ex4_14/github"
 )
+
+var issueList = template.Must(template.New("issuelist").Parse(`
+<h1>{{.TotalCount}} issues</h1>
+<table>
+<tr style='text-align: left'>
+  <th>#</th>
+  <th>State</th>
+  <th>User</th>
+  <th>Title</th>
+</tr>
+{{range .Items}}
+<tr>
+  <td><a href='{{.HTMLURL}}'>{{.Number}}</a></td>
+  <td>{{.State}}</td>
+  <td><a href='{{.User.HTMLURL}}'>{{.User.Login}}</a></td>
+  <td><a href='{{.HTMLURL}}'>{{.Title}}</a></td>
+</tr>
+{{end}}
+</table>
+`))
 
 //!+
 func main() {
@@ -42,13 +63,17 @@ func issue(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Fprintf(w, "%d issues:\n", result.TotalCount)
-	fmt.Fprintln(w, "\n<Bug List>")
+
+	//	fmt.Fprintf(w, "%d issues:\n", result.TotalCount)
+	//	fmt.Fprintln(w, "\n<Bug List>")
+	if err := issueList.Execute(w, result); err != nil {
+		log.Fatal(err)
+	}
 	milestones := make(map[string]string)
 	userNames := make(map[string]string)
 	for _, item := range result.Items {
-		fmt.Fprintf(w, "#%-5d %9.9s %.55s\n",
-			item.Number, item.User.Login, item.Title)
+		//		fmt.Fprintf(w, "#%-5d %9.9s %.55s\n",
+		//			item.Number, item.User.Login, item.Title)
 		if item.Milestone != nil {
 			milestones[item.Milestone.Title] = item.Milestone.Description
 		}
@@ -56,12 +81,14 @@ func issue(w http.ResponseWriter, r *http.Request) {
 			userNames[item.User.Login] = item.User.HTMLURL
 		}
 	}
-	fmt.Fprintln(w, "\n<Milestone List>")
-	for k, v := range milestones {
-		fmt.Fprintf(w, "%s\t%s\n", k, v)
-	}
-	fmt.Fprintln(w, "\n<User List>")
-	for k, v := range userNames {
-		fmt.Fprintf(w, "%s\t%s\n", k, v)
-	}
+	/*
+		fmt.Fprintln(w, "\n<Milestone List>")
+		for k, v := range milestones {
+					fmt.Fprintf(w, "%s\t%s\n", k, v)
+		}
+		fmt.Fprintln(w, "\n<User List>")
+		for k, v := range userNames {
+					fmt.Fprintf(w, "%s\t%s\n", k, v)
+		}
+	*/
 }

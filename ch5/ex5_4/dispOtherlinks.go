fe5e47c58d6fd00c671412f34eb76a143b4f4180
Esper0328 <esper0328@gmail.com>
2022-02-10 16:41:09 +0900

    displayed img links

---
+++ ch5/ex5_4/dispOtherlinks.go
@@ -0,0 +1,43 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 123.
+
+// Outline prints the outline of an HTML document tree.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+//!+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "outline: %v\n", err)
+		os.Exit(1)
+	}
+	visit(nil, doc)
+}
+
+func visit(stack []string, n *html.Node) {
+	if n.Type == html.ElementNode {
+		stack = append(stack, n.Data) // push tag
+		//		fmt.Println(stack)
+		if n.Data == "img" {
+			for _, a := range n.Attr {
+				if a.Key == "src" {
+					fmt.Println(a.Val)
+				}
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		visit(stack, c)
+	}
+}
+
+//!-

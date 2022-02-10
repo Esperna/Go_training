db4ed04cc375e70cfaa3626f8e50df42cd08f817
Esper0328 <esper0328@gmail.com>
2022-02-10 12:57:19 +0900

    Added setup script

---
+++ ch5/init.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+mkdir $1
+go build ./dispCurrentFolder.go
+var=$(pwd | xargs -I@ ./dispCurrentFolder @)
+cd $1
+cp ~/template/main.go ./
+go mod init $var/$1
+git add main.go go.mod
+git commit -m"initial commit for practice"

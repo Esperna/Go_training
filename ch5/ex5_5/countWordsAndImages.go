4e48f5b012a7a26c5f3dcf69ba472e1e90f4f84c
Esper0328 <esper0328@gmail.com>
2022-02-11 10:13:42 +0900

    called countWordsAndImages which is blank

--- ch5/ex5_5/countWordsAndImages.go
+++ ch5/ex5_5/countWordsAndImages.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"golang.org/x/net/html"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	words, images, _ := CountWordsAndImages(os.Args[1])
+	fmt.Printf("%v\t%v\n", words, images)
+}
+
+func CountWordsAndImages(url string) (words, images int, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		err = fmt.Errorf("parsing HML: %s", err)
+		return
+	}
+	words, images = countWordsAndImages(doc)
+	return
+}
+
+func countWordsAndImages(n *html.Node) (words, images int) {
+	return
 }

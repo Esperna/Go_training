29c25a1a6eabe80260515dda0649f469dba071a9
Esper0328 <esper0328@gmail.com>
2022-02-11 16:25:38 +0900

    stopping using redundant map

--- ch5/ex5_5/countWordsAndImages.go
+++ ch5/ex5_5/countWordsAndImages.go
@@ -24,7 +24,7 @@ func CountWordsAndImages(url string) (words, images int, err error) {
 	doc, err := html.Parse(resp.Body)
 	resp.Body.Close()
 	if err != nil {
-		err = fmt.Errorf("parsing HML: %s", err)
+		err = fmt.Errorf("parsing HTML: %s", err)
 		return
 	}
 	words, images = countWordsAndImages(doc)
@@ -32,25 +32,18 @@ func CountWordsAndImages(url string) (words, images int, err error) {
 }
 
 func countWordsAndImages(n *html.Node) (words, images int) {
-	wordfreq := make(map[string]int)
-	imagefreq := 0
-	wordfreq, imagefreq = wordAndImageFreq(nil, n, wordfreq, imagefreq)
-	for _, v := range wordfreq {
-		//fmt.Printf("%s\n", k)
-		words += v
-	}
-	images = imagefreq
+	words, images = wordAndImageFreq(nil, n)
 	return words, images
 }
 
-func wordAndImageFreq(stack []string, n *html.Node, seen map[string]int, images int) (map[string]int, int) {
+func wordAndImageFreq(stack []string, n *html.Node) (words int, images int) {
 	if n.Type == html.ElementNode {
 		stack = append(stack, n.Data) // push tag
 	}
 	if !(n.Data == "style" || n.Data == "script") {
 		if n.Type == html.TextNode {
 			//fmt.Printf("%s", n.Data)
-			seen = wordfreq(bytes.NewBufferString(n.Data), seen)
+			words += wordfreq(bytes.NewBufferString(n.Data))
 		}
 	}
 	if n.Data == "img" {
@@ -62,12 +55,15 @@ func wordAndImageFreq(stack []string, n *html.Node, seen map[string]int, images 
 		}
 	}
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		seen, images = wordAndImageFreq(stack, c, seen, images)
+		wordCount, imageCount := wordAndImageFreq(stack, c)
+		words += wordCount
+		images += imageCount
 	}
-	return seen, images
+	return words, images
 }
 
-func wordfreq(r io.Reader, seen map[string]int) map[string]int {
+func wordfreq(r io.Reader) (words int) {
+	seen := make(map[string]int)
 	input := bufio.NewScanner(r)
 	input.Split(bufio.ScanWords)
 	for input.Scan() {
@@ -78,5 +74,8 @@ func wordfreq(r io.Reader, seen map[string]int) map[string]int {
 		fmt.Fprintf(os.Stderr, "dedup: %v\n", err)
 		os.Exit(1)
 	}
-	return seen
+	for _, v := range seen {
+		words += v
+	}
+	return words
 }

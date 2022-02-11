8be0d1bc286e92774d9ed9f07eb66eba6c35a175
Esper0328 <esper0328@gmail.com>
2022-02-11 16:55:22 +0900

    deleted  unneeded struct definition

--- ch4/ex4_13/main.go
+++ ch4/ex4_13/main.go
@@ -10,36 +10,7 @@ import (
 )
 
 type Movie struct {
-	Title      string
-	Year       string
-	Rated      string
-	Released   string
-	Runtime    string
-	Genre      string
-	Director   string
-	Writer     string
-	Actors     string
-	Plot       string
-	Language   string
-	Country    string
-	Awards     string
-	Poster     string
-	Ratings    []Rating
-	Metascore  string
-	ImdbRating string `json:"imdbRating"`
-	ImdbVotes  string `json:"imdbVotes"`
-	ImdbID     string `json:"imdbID"`
-	Type       string
-	DVD        string
-	BoxOffice  string
-	Production string
-	Website    string
-	Response   string
-}
-
-type Rating struct {
-	Source string
-	Value  string
+	Poster string
 }
 
 func main() {

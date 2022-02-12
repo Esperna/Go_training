338e7bb796e297b08161e4a6d98f009459b2a0ff
Esper0328 <esper0328@gmail.com>
2022-02-12 15:40:22 +0900

    modified test case because of UTF-8 encoded strings

--- ch4/ex4_7/test.sh
+++ ch4/ex4_7/test.sh
@@ -1,13 +1,5 @@
 #!/bin/zsh
 
 go build ./main.go
-echo "./main a"
-./main a
-echo "./main abc"
-./main abc
-echo "./main abcde"
-./main abcde
-echo "./main abc123"
-./main abc123
-echo "./main a b c"
-./main a b c
+echo "./main 太平洋"
+./main 太平洋

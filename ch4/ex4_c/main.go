12ea13d52ce500f6a636edc95323aa545ac78431
Esper0328 <esper0328@gmail.com>
2022-02-12 11:37:12 +0900

    printed slice when coping slice in remove

--- ch4/ex4_c/main.go
+++ ch4/ex4_c/main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("vim-go")
+	s := []int{5, 6, 7, 8, 9}
+	fmt.Println(remove(s, 2))
+}
+
+func remove(slice []int, i int) []int {
+	copy(slice[i:], slice[i+1:])
+	fmt.Println(slice)
+	return slice[:len(slice)-1]
 }

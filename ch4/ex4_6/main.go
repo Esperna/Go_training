36f5b2397e9a610dadce1f717b2a571950d979a6
Esper0328 <esper0328@gmail.com>
2022-02-12 15:35:02 +0900

    compressed neibor space in orginal byte slice

--- ch4/ex4_6/main.go
+++ ch4/ex4_6/main.go
@@ -12,25 +12,69 @@ func main() {
 		fmt.Println("Invalid Number of Argument")
 		os.Exit(1)
 	}
-	r := []rune(os.Args[1])
-	r = compressUnicodeSpaces(r)
-	fmt.Println(string(r))
+	b := []byte(os.Args[1])
+	for i, r := range os.Args[1] {
+		fmt.Printf("%d\t%q\t%d\n", i, r, r)
+	}
+	b = compressUnicodeSpaces(b)
+	fmt.Println(string(b))
 }
 
-func compressUnicodeSpaces(s []rune) []rune {
+func compressUnicodeSpaces(b []byte) []byte {
+	for i := 0; i < len(b); {
+		isASCII := (b[i] >> 7) == 0
+		is2Byte := (b[i] >> 5) == 0b110
+		is3Byte := (b[i] >> 5) == 0b111
+		if isASCII {
+			if unicode.IsSpace(rune(b[i])) {
+				b[i] = 0x20 //不要？
+			}
+			i++
+		} else if is2Byte {
+			if i+1 < len(b) {
+				r := []rune(string(b[i : i+2]))
+				if unicode.IsSpace(r[0]) {
+					b = remove(b, i+1)
+					b[i] = 0x20
+					i++
+				} else {
+					i += 2
+				}
+			}
+		} else if is3Byte {
+			if i+2 < len(b) {
+				r := []rune(string(b[i : i+3]))
+				if unicode.IsSpace(r[0]) {
+					b = remove(b, i+2)
+					b = remove(b, i+1)
+					b[i] = 0x20
+					i++
+				} else {
+					i += 3
+				}
+			}
+		} else {
+			i++
+		}
+	}
+	b = deleteDupSpace(b)
+	return b
+}
+func deleteDupSpace(s []byte) []byte {
 	for i := 0; i < len(s); i++ {
 		if i+1 < len(s) {
-			if unicode.IsSpace(s[i+1]) && unicode.IsSpace(s[i]) {
+			r1 := []rune(string(s[i+1]))
+			r0 := []rune(string(s[i]))
+			if unicode.IsSpace(r1[0]) && unicode.IsSpace(r0[0]) {
 				s = remove(s, i)
-				s[i] = 0x20
-				s = compressUnicodeSpaces(s)
+				s = deleteDupSpace(s)
 			}
 		}
 	}
 	return s
 }
 
-func remove(slice []rune, i int) []rune {
+func remove(slice []byte, i int) []byte {
 	copy(slice[i:], slice[i+1:])
 	return slice[:len(slice)-1]
 }

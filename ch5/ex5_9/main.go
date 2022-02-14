3abe621d7e095e470805e5e50fc3d58b1bf5b5e2
Esper0328 <esper0328@gmail.com>
2022-02-14 22:29:27 +0900

    implemented based on assmuption that f(foo) returns length of foo

--- ch5/ex5_9/main.go
+++ ch5/ex5_9/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	length := len(os.Args)
+	if length != 2 {
+		fmt.Fprintf(os.Stderr, "expand failed: invalid num of args ")
+		os.Exit(1)
+	}
+	f := Length
+	fmt.Println(expand(os.Args[1], f))
+}
+
+func Length(s string) string {
+	return strconv.Itoa(len(s))
+}
+
+func expand(s string, f func(string) string) string {
+	slices := strings.Split(s, "$")
+	var str string
+	for _, item := range slices {
+		str += f(item)
+	}
+	if HasPrefix(s, "$") {
+		return str[1:]
+	} else {
+		return slices[0] + str[1:]
+	}
+}
+
+func HasPrefix(s, prefix string) bool {
+	return len(s) >= len(prefix) && s[:len(prefix)] == prefix
 }

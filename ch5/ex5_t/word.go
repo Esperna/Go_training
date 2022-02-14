063f192269ac51eedb6a3db53a2b08ea262cfe20
Esper0328 <esper0328@gmail.com>
2022-02-14 18:34:10 +0900

    practice of using test package

---
+++ ch5/ex5_t/word.go
@@ -0,0 +1,31 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 303.
+//!+
+
+// Package word provides utilities for word games.
+package word
+
+import (
+	"unicode"
+)
+
+// IsPalindrome reports whether s reads the same forward and backward.
+// (Our first attempt.)
+func IsPalindrome(s string) bool {
+	var letters []rune
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			letters = append(letters, unicode.ToLower(r))
+		}
+	}
+	for i := range letters {
+		if letters[i] != letters[len(letters)-1-i] {
+			return false
+		}
+	}
+	return true
+}
+
+//!-

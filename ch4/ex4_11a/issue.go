8ff5a829fc0f7e9990a88c06ec656128cf17c3ea
Esper0328 <esper0328@gmail.com>
2022-02-15 20:49:32 +0900

    refactoring

--- ch4/ex4_11a/issue.go
+++ ch4/ex4_11a/issue.go
@@ -12,37 +12,64 @@ import (
 
 func main() {
 	length := len(os.Args)
-	if length != 3 {
-		fmt.Fprint(os.Stderr, fmt.Sprintf("invalid number of args. issue issueNo editor is expected \n"))
+	if !(length == 3 || length == 4) {
+		fmt.Fprint(os.Stderr, fmt.Sprintf("invalid number of args. \n"))
+		displayHowToUse()
+		os.Exit(1)
+	}
+	if !(os.Args[1] == "-c" || os.Args[1] == "-r" || os.Args[1] == "-u" || os.Args[1] == "-d" || os.Args[1] == "-uc") {
+		fmt.Fprint(os.Stderr, fmt.Sprintf("invalid option. \n"))
+		displayHowToUse()
 		os.Exit(1)
 	}
 
-	cmd := exec.Command(os.Args[2], "tmp.txt")
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	err := cmd.Run()
-	if err != nil {
-		fmt.Fprint(os.Stderr, fmt.Sprintf("failed run command. %s\n", err.Error()))
-	}
-
-	content, err := ioutil.ReadFile("tmp.txt")
-	if err != nil {
-		fmt.Fprint(os.Stderr, fmt.Sprintf("failed read content. %s\n", err.Error()))
-		os.Exit(1)
-	}
-
-	jsonStr := string(content)
-	fmt.Printf("%s\n", jsonStr)
-
-	err = godotenv.Load("github.env")
+	err := godotenv.Load("github.env")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error loading .env file")
 		os.Exit(1)
 	}
 
-	issueNo := os.Args[1]
 	GitHubID := os.Getenv("GITHUB_ID")
 	Token := os.Getenv("GITHUB_TOKEN")
-	github.UpdateIssue(issueNo, GitHubID, Token, jsonStr)
 
+	if os.Args[1] == "-c" {
+
+	} else if os.Args[1] == "-r" {
+
+	} else if os.Args[1] == "-u" {
+		jsonStr, err := inputFromEditor(os.Args[3])
+		if err != nil {
+			fmt.Fprint(os.Stderr, "input from editor failed")
+			os.Exit(1)
+		}
+		issueNo := os.Args[2]
+		github.UpdateIssue(issueNo, GitHubID, Token, jsonStr)
+
+	} else if os.Args[1] == "-d" {
+
+	}
+}
+
+func inputFromEditor(editor string) (string, error) {
+	cmd := exec.Command(editor, "tmp.txt")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	err := cmd.Run()
+	if err != nil {
+		fmt.Fprint(os.Stderr, fmt.Sprintf("failed run command. %s\n", err.Error()))
+		return "", err
+	}
+	content, err := ioutil.ReadFile("tmp.txt")
+	if err != nil {
+		fmt.Fprint(os.Stderr, fmt.Sprintf("failed read content. %s\n", err.Error()))
+		return string(content), err
+	}
+	return string(content), nil
+}
+
+func displayHowToUse() {
+	fmt.Println("./issue -c editor")
+	fmt.Println("./issue -r")
+	fmt.Println("./issue -u IssueNo editor")
+	fmt.Println("./issue -d IssueNo")
 }

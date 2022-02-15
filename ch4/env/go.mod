f68dbc7d349b3df6ca5b376e324b3758befe6de3
Esper0328 <esper0328@gmail.com>
2022-02-15 18:45:03 +0900

    practice of how to use godotenv

--- ch4/env/go.mod
+++ ch4/env/go.mod
@@ -1,3 +1,5 @@
 module ch4/env
 
 go 1.17
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- ch4/env/main.go
+++ ch4/env/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	err := godotenv.Load("github.env")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading .env file")
+		os.Exit(1)
+	}
+	length := len(os.Args)
+	if length != 3 {
+		fmt.Fprintf(os.Stderr, "invalid number of args")
+		os.Exit(1)
+	}
+
+	//os.Setenv does not change .env file itself
+	err = os.Setenv("GITHUB_ID", os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "github id error %s", err.Error())
+	}
+	err = os.Setenv("GITHUB_TOKEN", os.Args[2])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "github token error %s", err.Error())
+	}
+
+	fmt.Printf("%s\n%s\n", os.Getenv("GITHUB_ID"), os.Getenv("GITHUB_TOKEN"))
 }

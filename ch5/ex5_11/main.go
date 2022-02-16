54ae292346628bbda9fce6baf875371b5a715494
Esper0328 <esper0328@gmail.com>
2022-02-16 16:50:42 +0900

    added cyclic check logic

--- ch5/ex5_11/main.go
+++ ch5/ex5_11/main.go
@@ -8,15 +8,16 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"sort"
 )
 
 //!+table
 // prereqs maps computer science courses to their prerequisites.
 var prereqs = map[string][]string{
-	"algorithms": {"data structures"},
-	"calculus":   {"linear algebra"},
-
+	"algorithms":     {"data structures"},
+	"calculus":       {"linear algebra"},
+	"linear algebra": {"calculus"},
 	"compilers": {
 		"data structures",
 		"formal languages",
@@ -44,16 +45,29 @@ func main() {
 func topoSort(m map[string][]string) []string {
 	var order []string
 	seen := make(map[string]bool)
-	var visitAll func(items []string)
+	counts := make(map[string]int)
+	var visitAll func(items []string) bool
 
-	visitAll = func(items []string) {
+	visitAll = func(items []string) bool {
 		for _, item := range items {
+			fmt.Fprintf(os.Stdout, "%s\n", item)
+			counts[item]++
+			if counts[item] > 1 {
+				fmt.Fprintf(os.Stderr, "graph is cyclic: %s appears again\n", item)
+				return false
+			}
 			if !seen[item] {
 				seen[item] = true
-				visitAll(m[item])
+				if !visitAll(m[item]) {
+					return false
+				}
 				order = append(order, item)
 			}
+			for k, _ := range counts {
+				counts[k] = 0
+			}
 		}
+		return true
 	}
 
 	var keys []string
@@ -62,7 +76,9 @@ func topoSort(m map[string][]string) []string {
 	}
 
 	sort.Strings(keys)
-	visitAll(keys)
+	if !visitAll(keys) {
+		return nil
+	}
 	return order
 }
 

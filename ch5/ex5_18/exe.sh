eca28ea6ed6f8bcc233cbfc0eea7caab03e2f7c8
Esper0328 <esper0328@gmail.com>
2022-02-16 18:28:46 +0900

    modified script for cleaning files after executing

--- ch5/ex5_18/exe.sh
+++ ch5/ex5_18/exe.sh
@@ -3,3 +3,5 @@ echo "go run ./fetch.go https://golang.org"
 go run ./fetch.go https://golang.org
 echo "diff index.html answer.html"
 diff index.html answer.html
+echo "rm index.html"
+rm index.html

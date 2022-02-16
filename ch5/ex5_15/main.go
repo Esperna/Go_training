d45c81eb709673ed159d53e9942ce6eb23429cf3
Esper0328 <esper0328@gmail.com>
2022-02-16 19:10:29 +0900

    implemented max including no argument case

--- ch5/ex5_15/main.go
+++ ch5/ex5_15/main.go
@@ -6,7 +6,11 @@
 // The sum program demonstrates a variadic function.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"os"
+)
 
 //!+
 func sum(vals ...int) int {
@@ -17,17 +21,48 @@ func sum(vals ...int) int {
 	return total
 }
 
+func max(vals ...int) int {
+	length := len(vals)
+	if length == 0 {
+		fmt.Fprintf(os.Stderr, "no vals:")
+		return 0
+	}
+	var max int = math.MinInt
+	for _, val := range vals {
+		if max < val {
+			max = val
+		}
+	}
+	return max
+}
+
 //!-
 
 func main() {
 	//!+main
-	fmt.Println(sum())           //  "0"
-	fmt.Println(sum(3))          //  "3"
+	fmt.Println("sum()")
+	fmt.Println(sum()) //  "0"
+	fmt.Println("sum(3)")
+	fmt.Println(sum(3)) //  "3"
+	fmt.Println("sum(1, 2, 3, 4)")
 	fmt.Println(sum(1, 2, 3, 4)) //  "10"
 	//!-main
 
 	//!+slice
+	fmt.Println("values := []int{1, 2, 3, 4}")
 	values := []int{1, 2, 3, 4}
+	fmt.Println("sum(values...)")
 	fmt.Println(sum(values...)) // "10"
 	//!-slice
+
+	fmt.Println("max()")
+	fmt.Println(max())
+	fmt.Println("max(5)")
+	fmt.Println(max(5))
+	fmt.Println("max(1,0,777,10)")
+	fmt.Println(max(1, 0, 777, 10))
+
+	fmt.Println("values := []int{1, 2, 3, 4}")
+	fmt.Println("max(values...)")
+	fmt.Println(max(values...)) // "10"
 }

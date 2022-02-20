e74f0ca1d0b919b02941a6d4582f2a9b9bde8ab1
Esper0328 <esper0328@gmail.com>
2022-02-20 12:05:30 +0900

    implemented variadic version of strings.Join

--- ch5/ex5_16/main.go
+++ ch5/ex5_16/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println(Join())
+	fmt.Println(Join("1", " "))
+	fmt.Println(Join("1", "2", " "))
+	fmt.Println(Join("abc", "cde", "fgh", " "))
+
+}
+
+func Join(elems ...string) string {
+	length := len(elems)
+	if length < 1 {
+		fmt.Fprintf(os.Stderr, "Invalid number of argument")
+		return ""
+	}
+	sep := elems[len(elems)-1]
+	sum := ""
+	for i, v := range elems {
+		sum += v
+		if i < len(elems)-1 {
+			sum += sep
+		}
+	}
+	return sum
 }

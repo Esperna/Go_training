f08dde32f8ac6ccbd23f75dc23e24494ac171d6c
Esper0328 <esper0328@gmail.com>
2022-02-21 21:00:19 +0900

    displayed layer of file system

--- ch5/ex5_14/main.go
+++ ch5/ex5_14/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	breadthFirst(crawl, os.Args[1:])
+}
+
+func breadthFirst(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+
+	}
+}
+
+func crawl(path string) []string {
+	if !strings.HasSuffix(path, "/") {
+		path += "/"
+	}
+	fmt.Println(path)
+	var list []string
+	dirs, err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ReadDir failed: %s", err)
+	}
+	for _, dir := range dirs {
+		if dir.IsDir() {
+			dirpath := path + dir.Name() + "/"
+			list = append(list, dirpath)
+			fmt.Printf("%s\n", dirpath)
+		} else {
+			fmt.Printf("%s%s\n", path, dir.Name())
+		}
+	}
+	return list
 }

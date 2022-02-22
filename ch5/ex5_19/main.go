920deaf0af1fc349925b749eb71293fb309b6615
Esper0328 <esper0328@gmail.com>
2022-02-22 10:40:27 +0900

    implemented function which returns non-zero though not including return statement

--- ch5/ex5_19/main.go
+++ ch5/ex5_19/main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println(Hello())
+}
+
+func Hello() (greeting string, err error) {
+	defer func() {
+		switch p := recover(); p {
+		case nil:
+			err = fmt.Errorf("within expected")
+			greeting = "Hello."
+		default:
+			panic(p)
+		}
+	}()
+	panic(nil)
 }

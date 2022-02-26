d37da9bf5917643a8c8c1806536a597683bcd6f3
Esper0328 <esper0328@gmail.com>
2022-02-26 18:05:17 +0900

    Added value check test

--- ch4/ex4_1/SHA256_test.go
+++ ch4/ex4_1/SHA256_test.go
@@ -21,5 +21,32 @@ func TestCountBitDiff(t *testing.T) {
 		if CountBitDiff(sum1, sum2) != test.want {
 			t.Errorf("countBitDiff(sum1, sum2) != 0. the value is %d\n %x\n%x\n", CountBitDiff(sum1, sum2), sum1, sum2)
 		}
+		if CountBitDiff(sum1, sum2) != popCountDiff(sum1, sum2) {
+			t.Errorf("countBitDiff(sum1, sum2) != popCountDiff(sum1,sum2). countBitDiff is %d popCountDiff is %d", CountBitDiff(sum1, sum2), popCountDiff(sum1, sum2))
+		}
 	}
+}
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func totalPopCount(bytes []byte) int {
+	total := 0
+	for _, b := range bytes {
+		total += int(pc[b])
+	}
+	return total
+}
+
+func popCountDiff(sum1, sum2 [sha256.Size]byte) int {
+	xorBytes := make([]byte, 0, sha256.Size)
+	for i := 0; i < sha256.Size; i++ {
+		xorBytes = append(xorBytes, sum1[i]^sum2[i])
+	}
+	return totalPopCount(xorBytes)
 }

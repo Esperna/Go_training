bba8b45eafcaedfd9546a80da297c468755ca2ac
Esper0328 <esper0328@gmail.com>
2022-02-27 13:45:17 +0900

    Added the case longer slice shifts more

--- ch4/ex4_4/main_test.go
+++ ch4/ex4_4/main_test.go
@@ -2,13 +2,14 @@ package main
 
 import "testing"
 
-func TestRotate(t *testing.T) {
+func TestLeftRotate(t *testing.T) {
 	var tests = []struct {
 		inputs    []int
 		rotNumber int
 		wants     []int
 	}{
 		{[]int{1, 2, 3}, 1, []int{2, 3, 1}},
+		{[]int{1, 2, 3, 4, 5, 6, 7, 8}, 5, []int{6, 7, 8, 1, 2, 3, 4, 5}},
 	}
 	for _, test := range tests {
 		outputs := rotate(test.inputs, test.rotNumber)

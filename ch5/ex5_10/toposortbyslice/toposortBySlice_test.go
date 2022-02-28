89677a8430a259b5f4df9c505c7bc3edfe10d36c
Esper0328 <esper0328@gmail.com>
2022-02-28 19:57:12 +0900

    Deleted unneeded comment out line

--- ch5/ex5_10/toposortbyslice/toposortBySlice_test.go
+++ ch5/ex5_10/toposortbyslice/toposortBySlice_test.go
@@ -3,18 +3,6 @@ package main
 import "testing"
 
 func TestToposortBySlice(t *testing.T) {
-	// var tests = []struct {
-	// 	input1, input2 []byte
-	// 	want           int
-	// }{
-	// 	{[]byte("x"), []byte("x"), 0},
-	// 	{[]byte("X"), []byte("X"), 0},
-	// 	{[]byte("HelloWorld"), []byte("HelloWorld"), 0},
-	// }
-	// for _, test := range tests {
-	// 	sum1 := sha256.Sum256(test.input1)
-	//
-	// }
 	isExpected := true
 	courses := topoSort(prereqs)
 	for i := 0; i < len(courses)-1; i++ {

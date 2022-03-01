323ec87e07042513b4ceff6ae30ba8778ddee08b
Esper0328 <esper0328@gmail.com>
2022-03-01 15:55:42 +0900

    copied from ex7_1

---
+++ ch7/ex7_2/counter.go
@@ -0,0 +1,64 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 173.
+
+// Bytecounter demonstrates an implementation of io.Writer that counts bytes.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type ByteCounter int
+
+func (c *ByteCounter) Write(p []byte) (int, error) {
+	*c += ByteCounter(len(p)) // convert int to ByteCounter
+	return len(p), nil
+}
+
+type WordCounter int
+
+func (c *WordCounter) Write(p []byte) (int, error) {
+	count := countByFunc(p, bufio.ScanWords)
+	*c += WordCounter(count)
+	return count, nil
+}
+
+type LineCounter int
+
+func (c *LineCounter) Write(p []byte) (int, error) {
+	count := countByFunc(p, bufio.ScanLines)
+	*c += LineCounter(count)
+	return count, nil
+}
+
+func countByFunc(p []byte, f bufio.SplitFunc) (count int) {
+	s := string(p)
+	scanner := bufio.NewScanner(strings.NewReader(s))
+	scanner.Split(f)
+	count = 0
+	for scanner.Scan() {
+		count++
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading input:", err)
+	}
+	return
+}
+
+func main() {
+	//!+main
+	var c ByteCounter
+	c.Write([]byte("hello"))
+	fmt.Println(c) // "5", = len("hello")
+
+	c = 0 // reset the counter
+	var name = "Dolly"
+	fmt.Fprintf(&c, "hello, %s", name)
+	fmt.Println(c) // "12", = len("hello, Dolly")
+	//!-main
+}
---
+++ ch7/ex7_2/counter_test.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"testing"
+)
+
+type CounterType int
+
+func TestCounterWrite(t *testing.T) {
+	const (
+		Byte CounterType = iota
+		Word
+		Line
+	)
+	var tests = []struct {
+		input     string
+		countType CounterType
+		want      int
+	}{
+		{"hello", Byte, 5},
+		{"The Yellow Monkey", Word, 3},
+		{"This is a pen.\nThis is an apple.\nAh, apple pen.\n", Line, 3},
+	}
+	//It's not counter value check but return value check
+	for _, test := range tests {
+		var length int
+		var err error
+		if test.countType == Byte {
+			var c ByteCounter
+			length, err = c.Write([]byte(test.input))
+		} else if test.countType == Word {
+			var c WordCounter
+			length, err = c.Write([]byte(test.input))
+		} else if test.countType == Line {
+			var c LineCounter
+			length, err = c.Write([]byte(test.input))
+		}
+		if err != nil {
+			t.Errorf("write failed. %s ", err)
+		}
+		if length != test.want {
+			t.Errorf("length != test.want. length:%d test.want:%d", length, test.want)
+		}
+	}
+}
+
+func TestByteCounterValue(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  int
+	}{
+		{"hello", 5},
+		{"hello", 10},
+		{"hello", 15},
+	}
+	var c ByteCounter
+	for _, test := range tests {
+		c.Write([]byte(test.input))
+		if c != ByteCounter(test.want) {
+			t.Errorf("count != test.want. count:%d test.want:%d", c, test.want)
+		}
+	}
+}
+
+func TestWordCounterValue(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  int
+	}{
+		{"The Yellow Monkey", 3},
+		{"The Yellow Monkey", 6},
+		{"The Yellow Monkey", 9},
+	}
+	var c WordCounter
+	for _, test := range tests {
+		c.Write([]byte(test.input))
+		if c != WordCounter(test.want) {
+			t.Errorf("count != test.want. count:%d test.want:%d", c, test.want)
+		}
+	}
+}
+
+func TestLineCounterValue(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  int
+	}{
+		{"This is a pen.\nThis is an apple.\nAh, apple pen.\n", 3},
+		{"This is a pen.\nThis is an apple.\nAh, apple pen.\n", 6},
+		{"This is a pen.\nThis is an apple.\nAh, apple pen.\n", 9},
+		{"This is a pen.\nThis is an apple.\nAh, apple pen.\n", 12},
+	}
+	var c LineCounter
+	for _, test := range tests {
+		c.Write([]byte(test.input))
+		if c != LineCounter(test.want) {
+			t.Errorf("count != test.want. count:%d test.want:%d", c, test.want)
+		}
+	}
+}

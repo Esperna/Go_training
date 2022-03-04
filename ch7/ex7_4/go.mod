9b4f4aa9a850bf40ff69fc34dfee4a74453ccab4
Esper0328 <esper0328@gmail.com>
2022-03-04 15:14:28 +0900

    implemented NewReader

--- ch7/ex7_4/go.mod
+++ ch7/ex7_4/go.mod
@@ -1,3 +1,5 @@
 module ch7/ex7_4
 
 go 1.17
+
+require golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
--- ch7/ex7_4/main.go
+++ ch7/ex7_4/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
+
+type Reader struct {
+	s string
+	i int64
+}
 
 func main() {
-	fmt.Println("vim-go")
+	rd := NewReader("<html><title>under construction</title></html>")
+	doc, err := html.Parse(rd)
+	if err != nil {
+		fmt.Errorf("html parse failed:%s", err)
+	}
+	fmt.Printf("First node type: %d\n", doc.Type)
+
+}
+
+func NewReader(s string) *Reader {
+	var rd Reader
+	rd = Reader{s, 0}
+	return &rd
+}
+
+func (r *Reader) Read(b []byte) (n int, err error) {
+	if r.i >= int64(len(r.s)) {
+		return 0, io.EOF
+	}
+	length := len(b)
+	if length == 0 {
+		return 0, nil
+	} else if length < 0 {
+		err = fmt.Errorf("invalid byte length")
+		return n, err
+	}
+
+	r.s = string(b)
+	r.i += int64(len(b))
+	return n, err
 }

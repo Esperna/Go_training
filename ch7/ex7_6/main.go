60aea73f637b9d04d07e9e360f7353a33b2d27a0
Esper0328 <esper0328@gmail.com>
2022-03-05 10:46:57 +0900

    flag practice

--- ch7/ex7_6/main.go
+++ ch7/ex7_6/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	//var n = flag.Bool("n", false, "omit trailing newline")
+	var n bool
+	flag.BoolVar(&n, "n", false, "omit trailing newline")
+	var sep = flag.String("s", " ", "separator")
+
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	if !n {
+		fmt.Println()
+	}
 }

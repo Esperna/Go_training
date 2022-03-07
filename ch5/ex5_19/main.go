d3b9a751ad8a01c7d9b2c2b639fa312c3eb25a06
Esper0328 <esper0328@gmail.com>
2022-03-07 14:42:48 +0900

    modified by using bailout

--- ch5/ex5_19/main.go
+++ ch5/ex5_19/main.go
@@ -7,14 +7,17 @@ func main() {
 }
 
 func Hello() (greeting string, err error) {
+	type bailout struct{}
 	defer func() {
 		switch p := recover(); p {
 		case nil:
+			//no panic
+		case bailout{}:
 			err = fmt.Errorf("within expected")
 			greeting = "Hello."
 		default:
 			panic(p)
 		}
 	}()
-	panic(nil)
+	panic(bailout{})
 }

d9ab3deaad84c95657b2fcc20ee356880902cbd1
Esper0328 <esper0328@gmail.com>
2022-03-08 22:05:04 +0900

    multi dimensional slice sort

--- ch7/ex7_8/main.go
+++ ch7/ex7_8/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	scores := [][]int{
+		{80, 70, 95},
+		{50, 90, 70},
+		{80, 90, 70},
+		{100, 50, 65},
+	}
+	fmt.Println(scores)
+	sort.Sort(TestScore(scores))
+	fmt.Println(scores)
+}
+
+type TestScore [][]int
+
+func (s TestScore) Len() int {
+	return len(s)
+}
+
+var FirstKey = 0
+var SecondKey = 1
+
+func (s TestScore) Less(i, j int) bool {
+	if s[i][FirstKey] < s[j][FirstKey] {
+		return true
+	} else if s[i][FirstKey] == s[j][FirstKey] {
+		return s[i][SecondKey] < s[j][SecondKey]
+	} else {
+		return false
+	}
+}
+
+func (s TestScore) Swap(i, j int) {
+	s[i][FirstKey], s[j][FirstKey] = s[j][FirstKey], s[i][FirstKey]
 }

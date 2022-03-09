3fd2f8118b55b20e695670b7b4bf21256be0506d
Esper0328 <esper0328@gmail.com>
2022-03-09 15:16:29 +0900

    just practice for using MultiReader

--- ch8/ex8_0/main.go
+++ ch8/ex8_0/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	r1 := strings.NewReader("first reader ")
+	r2 := strings.NewReader("second reader ")
+	r3 := strings.NewReader("third reader\n")
+	r := io.MultiReader(r1, r2, r3)
+
+	if _, err := io.Copy(os.Stdout, r); err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < 100; i++ {
+		fmt.Printf("\r%d", i)
+		time.Sleep(time.Second * 1)
+	}
 }

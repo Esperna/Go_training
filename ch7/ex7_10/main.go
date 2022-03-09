43106dbd47e662c795671dd25ccbc3b5af5157a6
Esper0328 <esper0328@gmail.com>
2022-03-09 17:33:56 +0900

    added test for customSort struct

--- ch7/ex7_10/main.go
+++ ch7/ex7_10/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"sort"
+	"time"
 )
-
-func main() {
-	fmt.Println("vim-go")
-}
 
 type IntSlice []int
 
@@ -21,6 +17,41 @@ func (s StringSlice) Len() int           { return len(s) }
 func (s StringSlice) Less(i, j int) bool { return s[i] < s[j] }
 func (s StringSlice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 
+type customSort struct {
+	t []Track
+}
+
+func (x customSort) Len() int { return len(x.t) }
+func (x customSort) Less(i, j int) bool {
+	if x.t[i].Title != x.t[j].Title {
+		return x.t[i].Title < x.t[j].Title
+	}
+	if x.t[i].Year != x.t[j].Year {
+		return x.t[i].Year < x.t[j].Year
+	}
+	if x.t[i].Length != x.t[j].Length {
+		return x.t[i].Length < x.t[j].Length
+	}
+	return false
+}
+func (x customSort) Swap(i, j int) { x.t[i], x.t[j] = x.t[j], x.t[i] }
+
+type Track struct {
+	Title  string
+	Artist string
+	Album  string
+	Year   int
+	Length time.Duration
+}
+
+func length(s string) time.Duration {
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		panic(s)
+	}
+	return d
+}
+
 func IsPalindrome(s sort.Interface) bool {
 	for i, j := 0, s.Len()-1; i < j; i, j = i+1, j-1 {
 		if !(!s.Less(i, j) && !s.Less(j, i)) {
@@ -28,4 +59,8 @@ func IsPalindrome(s sort.Interface) bool {
 		}
 	}
 	return true
+}
+
+func main() {
+
 }
--- ch7/ex7_10/main_test.go
+++ ch7/ex7_10/main_test.go
@@ -14,6 +14,24 @@ func TestIsPalindrome(t *testing.T) {
 		{false, IntSlice{1, 2, 3}},
 		{true, StringSlice{"time", "after", "time"}},
 		{false, StringSlice{"time", "goes", "by"}},
+		{true, customSort{
+			[]Track{
+				{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+				{"Go", "Moby", "Moby", 1992, length("3m37s")},
+				{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+				{"Go", "Moby", "Moby", 1992, length("3m37s")},
+				{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+			},
+		},
+		},
+		{false, customSort{
+			[]Track{
+				{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+				{"Go", "Moby", "Moby", 1992, length("3m37s")},
+				{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
+			},
+		},
+		},
 	}
 	for _, test := range tests {
 		actual := IsPalindrome(test.given)

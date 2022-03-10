860328bcbd49c8f07fc6722cccdcd2afb936e9ac
Esper0328 <esper0328@gmail.com>
2022-03-10 16:29:47 +0900

    added delete with not specified item

--- ch7/ex7_11/exe.sh
+++ ch7/ex7_11/exe.sh
@@ -30,4 +30,6 @@ Command="fetch http://localhost:8000/update?item=socks&price=-10"
 CommandWithEcho
 Command="fetch http://localhost:8000/update?item=socks&price=10yen"
 CommandWithEcho
+Command="fetch http://localhost:8000/delete"
+CommandWithEcho
 

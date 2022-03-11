8e338a602a887e9a82ac549338f03f5924d27b21
Esper0328 <esper0328@gmail.com>
2022-03-11 14:28:17 +0900

    Forgot to commit

---
+++ ch7/ex7_13/string.go
@@ -0,0 +1,21 @@
+package eval
+
+func (v Var) String() string {
+	return ""
+}
+
+func (l literal) String() string {
+	return ""
+}
+
+func (u unary) String() string {
+	return ""
+}
+
+func (b binary) String() string {
+	return ""
+}
+
+func (c call) String() string {
+	return ""
+}

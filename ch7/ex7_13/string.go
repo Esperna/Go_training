cba616821cce0f3409875845022a92d10e43e44a
Esper0328 <esper0328@gmail.com>
2022-03-11 15:22:21 +0900

    implemented String Interface.

--- ch7/ex7_13/string.go
+++ ch7/ex7_13/string.go
@@ -1,21 +1,35 @@
 package eval
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func (v Var) String() string {
-	return ""
+	return string(v)
 }
 
 func (l literal) String() string {
-	return ""
+	return strconv.Itoa(int(float64(l)))
 }
 
 func (u unary) String() string {
-	return ""
+	return fmt.Sprintf("(%c%s)", u.op, u.x)
 }
 
 func (b binary) String() string {
-	return ""
+	return fmt.Sprintf("(%s %c %s)", b.x, b.op, b.y)
 }
 
 func (c call) String() string {
-	return ""
+	str := c.fn
+	str += "("
+	for i := 0; i < len(c.args); i++ {
+		if i > 0 {
+			str += ","
+		}
+		str += fmt.Sprintf("%s", c.args[i])
+	}
+	str += ")"
+	return str
 }

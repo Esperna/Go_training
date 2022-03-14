a8d4873de65cd2b33ac5594736456b362cbc8b90
Esper0328 <esper0328@gmail.com>
2022-03-14 17:15:33 +0900

    implemented LimitReader

--- ch7/ex7_5/main.go
+++ ch7/ex7_5/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+type Reader struct {
+	i    int64
+	size int64
+	r    io.Reader
+}
 
 func main() {
-	fmt.Println("vim-go")
+	r := strings.NewReader("Hello Hello")
+	//lr := io.LimitReader(r, 6)
+	lr := LimitReader(r, 6)
+
+	if _, err := io.Copy(os.Stdout, lr); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func LimitReader(r io.Reader, n int64) io.Reader {
+	return &Reader{0, n, r}
+}
+
+func (r *Reader) Read(p []byte) (n int, err error) {
+	if r.i >= r.size {
+		return n, io.EOF
+	}
+	n, _ = r.r.Read(p)
+	if int64(n) > r.size {
+		n = int(r.size)
+	}
+	r.i += int64(n)
+	return n, err
 }

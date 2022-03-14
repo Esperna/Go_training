f085f3cb8fd251c8ccb601906ef903cb02e81350
Esper0328 <esper0328@gmail.com>
2022-03-14 22:06:11 +0900

    implemented reading a single expression from the standard input

---
+++ ch7/ex7_15/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"ch7/ex7_15/eval"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	fmt.Print("Input your expression>")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		scanner.Scan()
+		in := scanner.Text()
+		expr, err := eval.Parse(in)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+			continue
+		}
+		vars := make(map[eval.Var]bool)
+		if err := expr.Check(vars); err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+			continue
+		}
+		env := make(map[eval.Var]float64)
+		for k := range vars {
+			fmt.Printf("%s=", k)
+			scanner.Scan()
+			in := scanner.Text()
+			val, err := strconv.ParseFloat(in, 64)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s\n", err)
+				continue
+			}
+			env[k] = val
+		}
+		result := expr.Eval(env)
+		fmt.Println(result)
+		switch in {
+		default:
+			fmt.Print("Input your expression>")
+			continue
+		}
+	}
+}

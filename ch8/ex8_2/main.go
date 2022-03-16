21790e5e2cfbdde6d0257cc50763896c4ec6c792
Esper0328 <esper0328@gmail.com>
2022-03-16 16:36:46 +0900

    implemented Connection Establishment

--- ch8/ex8_2/main.go
+++ ch8/ex8_2/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	listener, err := net.Listen("tcp", "localhost:21")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(c net.Conn) {
+	fmt.Printf("Accept %v\n", c.RemoteAddr())
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, "220 Service ready for new user.\n")
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
 }

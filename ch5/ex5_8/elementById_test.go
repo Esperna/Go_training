0d8df290bc327f735a2546f7a49018b28e823560
Esper0328 <esper0328@gmail.com>
2022-03-18 19:43:32 +0900

    refactored elementById

--- ch5/ex5_8/elementById_test.go
+++ ch5/ex5_8/elementById_test.go
@@ -6,20 +6,23 @@ import (
 	"golang.org/x/net/html"
 )
 
-func TestStartElement(t *testing.T) {
+func TestElementById(t *testing.T) {
 	var tests = []struct {
 		node     html.Node
 		id       string
 		expected bool
 	}{
-		{html.Node{nil, nil, nil, nil, nil, html.ElementNode, 0, "", "", []html.Attribute{html.Attribute{"html", "lang", "en"}}}, "lang", true},
-		{html.Node{nil, nil, nil, nil, nil, html.ElementNode, 0, "", "", []html.Attribute{html.Attribute{"html", "lang", "en"}}}, "id", false},
-		{html.Node{nil, nil, nil, nil, nil, html.DoctypeNode, 0, "", "", []html.Attribute{html.Attribute{"", "", ""}}}, "lang", false},
+		{html.Node{Parent: nil, FirstChild: nil, LastChild: nil, PrevSibling: nil, NextSibling: nil, Type: html.ElementNode, DataAtom: 0, Data: "", Namespace: "",
+			Attr: []html.Attribute{html.Attribute{Namespace: "html", Key: "lang", Val: "en"}}}, "lang", true},
+		{html.Node{Parent: nil, FirstChild: nil, LastChild: nil, PrevSibling: nil, NextSibling: nil, Type: html.ElementNode, DataAtom: 0, Data: "", Namespace: "",
+			Attr: []html.Attribute{html.Attribute{Namespace: "html", Key: "lang", Val: "en"}}}, "id", false},
+		{html.Node{Parent: nil, FirstChild: nil, LastChild: nil, PrevSibling: nil, NextSibling: nil, Type: html.DoctypeNode, DataAtom: 0, Data: "", Namespace: "",
+			Attr: []html.Attribute{html.Attribute{Namespace: "", Key: "", Val: ""}}}, "lang", false},
 	}
 	for _, test := range tests {
-		result := startElement(&test.node, test.id)
-		if result != test.expected {
-			t.Errorf("result is not expected. result is %t. expected is %t.", result, test.expected)
+		result := ElementByID(&test.node, test.id)
+		if result != &test.node {
+			t.Errorf("result is not expected. result is %v. expected is %v.", result, &test.node)
 		}
 	}
 }

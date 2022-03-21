31835c1823f7a3d7c73a1a0001b84ef2bf76b409
Esper0328 <esper0328@gmail.com>
2022-03-21 13:49:30 +0900

    Added unit test for customSort.go

---
+++ ch7/ex7_8/customSort_test.go
@@ -0,0 +1,35 @@
+package main
+
+import "testing"
+
+func TestCustomSortBy1st2ndKey(t *testing.T) {
+	var tests = []struct {
+		key1, key2 string
+		expected   []*Track
+	}{
+		{"Title", "Artist", []*Track{
+			{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+			{"Go", "Moby", "Moby", 1992, length("3m37s")},
+			{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
+			{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
+		}},
+	}
+	for _, test := range tests {
+		actual := customSortBy1st2ndKey(test.key1, test.key2)
+		if !equal(test.expected, actual) {
+			t.Errorf("Not expected.  expected %v, actual %v", test.expected, actual)
+		}
+	}
+}
+
+func equal(x, y []*Track) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for i := range x {
+		if x[i].Title != y[i].Title || x[i].Artist != y[i].Artist || x[i].Album != y[i].Album || x[i].Year != y[i].Year || x[i].Length != y[i].Length {
+			return false
+		}
+	}
+	return true
+}

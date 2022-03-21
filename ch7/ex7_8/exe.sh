ab77fa420da7ee9d423c59168a83ff45aa6dc8d9
Esper0328 <esper0328@gmail.com>
2022-03-21 12:59:16 +0900

    Addeds test cases of 1st 2nd key sort

--- ch7/ex7_8/exe.sh
+++ ch7/ex7_8/exe.sh
@@ -6,3 +6,13 @@ CommandWithEcho(){
 
 Command="go run main.go -k1 Title -k2 Artist"
 CommandWithEcho
+Command="go run main.go -k1 Title -k2 Album"
+CommandWithEcho
+Command="go run main.go -k1 Title -k2 Year"
+CommandWithEcho
+Command="go run main.go -k1 Title -k2 Length"
+CommandWithEcho
+Command="go run main.go -k1 Length -k2 Year"
+CommandWithEcho
+Command="go run main.go -k1 Album -k2 Year"
+CommandWithEcho

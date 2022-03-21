7ea06b8dcd0c4b3de9677e4c4026e53c25d0074f
Esper0328 <esper0328@gmail.com>
2022-03-21 13:57:58 +0900

    Added test cases for 1st 2nd key sort

--- ch7/ex7_8/customSort_test.go
+++ ch7/ex7_8/customSort_test.go
@@ -13,6 +13,30 @@ func TestCustomSortBy1st2ndKey(t *testing.T) {
 			{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
 			{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
 		}},
+		{"Title", "Album", []*Track{
+			{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+			{"Go", "Moby", "Moby", 1992, length("3m37s")},
+			{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
+			{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
+		}},
+		{"Title", "Year", []*Track{
+			{"Go", "Moby", "Moby", 1992, length("3m37s")},
+			{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+			{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
+			{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
+		}},
+		{"Length", "Year", []*Track{
+			{"Go", "Moby", "Moby", 1992, length("3m37s")},
+			{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+			{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
+			{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
+		}},
+		{"Album", "Year", []*Track{
+			{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
+			{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+			{"Go", "Moby", "Moby", 1992, length("3m37s")},
+			{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
+		}},
 	}
 	for _, test := range tests {
 		actual := customSortBy1st2ndKey(test.key1, test.key2)

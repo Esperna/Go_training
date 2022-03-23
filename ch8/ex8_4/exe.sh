a842ae3135d5f31fed0e9d751be9e8d02e54c3c7
Esper0328 <esper0328@gmail.com>
2022-03-23 13:18:36 +0900

    refactored by using echo func in handleConn

--- ch8/ex8_4/exe.sh
+++ ch8/ex8_4/exe.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 killall reverb3
-go run reverb3.go &
+#go run reverb3.go &
+go build reverb3.go
+./reverb3 &
 go build ../ex8_3/netcat4.go
 ./netcat4
--- ch8/ex8_4/reverb3.go
+++ ch8/ex8_4/reverb3.go
@@ -21,14 +21,15 @@ func handleConn(c net.Conn) {
 	var wg sync.WaitGroup
 	for input.Scan() {
 		wg.Add(1)
-		go func(c net.Conn, shout string, delay time.Duration) {
+		echo := func(c net.Conn, shout string, delay time.Duration) {
 			defer wg.Done()
 			fmt.Fprintln(c, "\t", strings.ToUpper(shout))
 			time.Sleep(delay)
 			fmt.Fprintln(c, "\t", shout)
 			time.Sleep(delay)
 			fmt.Fprintln(c, "\t", strings.ToLower(shout))
-		}(c, input.Text(), 1*time.Second)
+		}
+		go echo(c, input.Text(), 1*time.Second)
 	}
 	wg.Wait()
 	// NOTE: ignoring potential errors from input.Err()

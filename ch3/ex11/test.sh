869b281080a78a164e84e2a6e5c8ea0e0a751a77
Esper0328 <esper0328@gmail.com>
2022-03-26 11:52:02 +0900

    renamed file from main.go to comma.go

--- ch3/ex11/test.sh
+++ ch3/ex11/test.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-go build ./main.go
+go build ./comma.go
 echo "Input->Expected:"
 echo "1              -> 1"
 echo "1.2            -> 1.2"
@@ -20,19 +20,19 @@ echo ".123           -> .123"
 echo ".1234          -> .1234"
 echo ""
 echo "Actual:"
-./main 1
-./main 1.2
-./main 123
-./main 1234
-./main 12345
-./main 12345.
-./main 123456
-./main 123456.789
-./main 1234567
-./main 1234567.8
-./main 1234567.89
-./main +1234567.89
-./main -123.456789
-./main .12
-./main .123
-./main .1234
+./comma 1
+./comma 1.2
+./comma 123
+./comma 1234
+./comma 12345
+./comma 12345.
+./comma 123456
+./comma 123456.789
+./comma 1234567
+./comma 1234567.8
+./comma 1234567.89
+./comma +1234567.89
+./comma -123.456789
+./comma .12
+./comma .123
+./comma .1234

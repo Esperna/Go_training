8d145a9d4249e1b1ac5137b364f717ff0f37abd0
Esper0328 <esper0328@gmail.com>
2022-03-27 14:13:16 +0900

    implemented concurrent mirror

--- ch8/ex07/exe.sh
+++ ch8/ex07/exe.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
-go build findlinks.go
-./findlinks -depth=1 https://www.google.com
+go build mirror.go
+./mirror -depth=1 https://www.google.com
 #./findlinks -depth=1 https://yahoo.co.jp
 #./findlinks -depth=1 http://golang.org
 #./findlinks -depth=1 http://gopl.io
--- ch8/ex07/mirror.go
+++ ch8/ex07/mirror.go
@@ -13,8 +13,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"strings"
 
 	"gopl.io/ch5/links"
 )
@@ -26,14 +30,42 @@ type urlInfo struct {
 
 var tokens = make(chan struct{}, 10)
 
-func crawl(url string, depth int) *urlInfo {
+func crawl(urlStr string, depth int) *urlInfo {
 	tokens <- struct{}{}
-	list, err := links.Extract(url)
+	list, err := links.Extract(urlStr)
 	<-tokens
 	if err != nil {
-		log.Print(err)
+		log.Printf("failed to extrackt %s: %s", urlStr, err)
 	}
-	fmt.Printf("depth %d %s\n", depth, url)
+	for _, item := range list {
+		if strings.HasPrefix(item, urlStr) {
+			extractUrl, err := url.Parse(item)
+			if err != nil {
+				log.Printf("failed to parse: %s", err)
+				continue
+			}
+			slashSplitDir := strings.Split(extractUrl.Path, "/")
+			path := extractUrl.Hostname()
+			for _, dir := range slashSplitDir[1:] {
+				if strings.HasSuffix(dir, ".html") {
+					break
+				}
+				path += "/" + dir
+				if err := makeDirIfNotExist(path); err != nil {
+					log.Printf("failed to make directory: %s", err)
+				}
+			}
+			fmt.Printf("path: %s\n", path)
+			if !strings.HasSuffix(path, ".html") {
+				path += "/index.html"
+			}
+			if err := download(item, path); err != nil {
+				log.Printf("failed to download %s to %s: %s", item, path, err)
+				continue
+			}
+		}
+	}
+	fmt.Printf("depth %d %s\n", depth, urlStr)
 	return &urlInfo{list, depth + 1}
 }
 
@@ -61,6 +93,20 @@ func main() {
 				if list.depth > *depthLimit {
 					break
 				}
+				pURL, err := url.Parse(link)
+				if err != nil {
+					log.Printf("failed to parse %s: %s", link, err)
+					continue
+				}
+				hostname := pURL.Hostname()
+				if err := makeDirIfNotExist(hostname); err != nil {
+					log.Printf("failed to make directory: %s", err)
+				}
+				path := hostname + "/index.html"
+				if err := download(link, path); err != nil {
+					log.Printf("failed to download %s to %s: %s", link, path, err)
+					continue
+				}
 				n++
 				go func(link string, depth int) {
 					worklist <- *crawl(link, depth)
@@ -69,4 +115,40 @@ func main() {
 		}
 	}
 	fmt.Printf("Finish!!\n")
+}
+
+func makeDirIfNotExist(dirName string) error {
+	if _, err := os.Stat(dirName); os.IsNotExist(err) {
+		err = os.Mkdir(dirName, 0644)
+		if err != nil {
+			log.Printf("failed to make %s: %v", dirName, err)
+			return err
+		}
+		err = os.Chmod(dirName, 0744)
+		if err != nil {
+			log.Printf("failed to change permmision of %s: %v", dirName, err)
+			return err
+		}
+	}
+	return nil
+}
+
+func download(urlStr, path string) error {
+	resp, err := http.Get(urlStr)
+	if err != nil {
+		return fmt.Errorf("failed to Get %s: %s", urlStr, err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to Get %s: %s", urlStr, resp.Status)
+	}
+	b, err := io.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		return fmt.Errorf("failed to read %s: %s", urlStr, err)
+	}
+	err = os.WriteFile(path, b, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to write %s: %s", path, err)
+	}
+	return nil
 }

5155a4392329721ab1c9f814b48a100a4174b32d
Esper0328 <esper0328@gmail.com>
2022-04-06 21:55:55 +0900

    refactored NOT logic

--- ch7/ex10/main.go
+++ ch7/ex10/main.go
@@ -54,7 +54,7 @@ func length(s string) time.Duration {
 
 func IsPalindrome(s sort.Interface) bool {
 	for i, j := 0, s.Len()-1; i < j; i, j = i+1, j-1 {
-		if !(!s.Less(i, j) && !s.Less(j, i)) {
+		if s.Less(i, j) || s.Less(j, i) {
 			return false
 		}
 	}

82f86e0c880231411d1736f5461df8f809148227
Esper0328 <esper0328@gmail.com>
2022-04-08 20:50:00 +0900

    modified TestCharCount because of changing return value of countChar

--- ch11/ex01/main.go
+++ ch11/ex01/main.go
@@ -17,9 +17,10 @@ import (
 )
 
 type charCount struct {
-	counts int
-	utflen int
-	err    error
+	counts  map[rune]int
+	utflen  [utf8.UTFMax + 1]int
+	invalid int
+	err     error
 }
 
 func main() {
@@ -60,7 +61,28 @@ func main() {
 }
 
 func countChar(rd io.Reader) charCount {
-	return charCount{3, 3, nil}
+	counts := make(map[rune]int)    // counts of Unicode characters
+	var utflen [utf8.UTFMax + 1]int // count of lengths of UTF-8 encodings
+	invalid := 0                    // count of invalid UTF-8 characters
+
+	in := bufio.NewReader(rd)
+	for {
+		r, n, err := in.ReadRune() // returns rune, nbytes, error
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "charcount: %v\n", err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+		counts[r]++
+		utflen[n]++
+	}
+	return charCount{counts, utflen, invalid, nil}
 }
 
 //!-
--- ch11/ex01/main_test.go
+++ ch11/ex01/main_test.go
@@ -1,26 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"testing"
 )
 
 func TestCharCount(t *testing.T) {
-	var tests = []struct {
-		expected charCount
-		given    string
-	}{
-		{charCount{3, 3, nil}, "in1"},
+	given := "in1"
+	f, err := os.Open(given)
+	if err != nil {
+		t.Errorf("%s", err)
 	}
-	for _, test := range tests {
-		f, err := os.Open(test.given)
-		if err != nil {
-			t.Errorf("%s", err)
-		}
-		actual := countChar(f)
-		if actual != test.expected {
-			t.Errorf("(%s): expected %v, actual %v", test.given, test.expected, actual)
+	actual := countChar(f)
+	for c, n := range actual.counts {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+	for c := range actual.counts {
+		if actual.counts[c] != 1 {
+			t.Errorf("counts is not expected. actual %v", actual.counts)
 		}
 	}
-
+	if actual.utflen[0] != 0 || actual.utflen[1] != 4 || actual.utflen[2] != 0 || actual.utflen[3] != 0 {
+		t.Errorf("utflen is not expected. actual %v", actual.utflen)
+	}
+	if actual.invalid != 0 {
+		t.Errorf("invalid is not expected. actual %v", actual.invalid)
+	}
 }

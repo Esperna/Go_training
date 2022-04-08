2aa426c4bb0bdcdf754de758b6301600291e4b06
Esper0328 <esper0328@gmail.com>
2022-04-08 21:43:43 +0900

    implemented invalid rune case

--- ch11/ex01/main_test.go
+++ ch11/ex01/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"testing"
@@ -25,6 +26,25 @@ func TestCharCount(t *testing.T) {
 		t.Errorf("utflen is not expected. actual %v", actual.utflen)
 	}
 	if actual.invalid != 0 {
+		t.Errorf("invalid is not expected. actual %v", actual.invalid)
+	}
+}
+
+func TestCharCountInvalid(t *testing.T) {
+	rd := bytes.NewReader([]byte{0xff})
+	actual := countChar(rd)
+	for c, n := range actual.counts {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+	for c := range actual.counts {
+		if actual.counts[c] != 1 {
+			t.Errorf("counts is not expected. actual %v", actual.counts)
+		}
+	}
+	if actual.utflen[0] != 0 || actual.utflen[1] != 0 || actual.utflen[2] != 0 || actual.utflen[3] != 0 {
+		t.Errorf("utflen is not expected. actual %v", actual.utflen)
+	}
+	if actual.invalid != 1 {
 		t.Errorf("invalid is not expected. actual %v", actual.invalid)
 	}
 }

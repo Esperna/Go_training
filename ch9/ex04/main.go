529680aaad1bce49cfde180922f4a41174516c53
Esper0328 <esper0328@gmail.com>
2022-04-13 14:54:07 +0900

    implemented goroutine pipeline for any specified value

--- ch9/ex04/main.go
+++ ch9/ex04/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+var n = flag.Int("n", 1, "number of go routine")
 
 func main() {
-	fmt.Println("vim-go")
+	flag.Parse()
+	fmt.Printf("number of goroutine is %d\n", *n)
+
+	channels := make([]chan int, *n)
+	for i := 0; i < *n; i++ {
+		channels[i] = make(chan int)
+	}
+
+	go func() {
+		for x := 0; ; x++ {
+			channels[0] <- x
+		}
+	}()
+	for i := 0; i < *n-1; i++ {
+		go func(i int) {
+			for {
+				x := <-channels[i]
+				channels[i+1] <- x + 1
+			}
+		}(i)
+	}
+
+	for x := range channels[*n-1] {
+		fmt.Println(x)
+	}
 }

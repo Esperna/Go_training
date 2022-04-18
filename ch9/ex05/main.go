10e8d5440a37533ccf9d1687f5b9bf8350d6e153
Esper0328 <esper0328@gmail.com>
2022-04-18 21:08:26 +0900

    implemented ping-pong display among 2 goroutines

--- ch9/ex05/main.go
+++ ch9/ex05/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	channelA := make(chan int)
+	channelB := make(chan int)
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for x := 0; x < 100; x++ {
+			channelA <- x
+			fmt.Printf("Ping%d\n", x)
+			x = <-channelB
+		}
+		close(channelA)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for x := range channelA {
+			fmt.Printf("Pong%d\n", x)
+			x++
+			channelB <- x
+		}
+	}()
+	wg.Wait()
 }

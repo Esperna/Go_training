98efee1c9cb8e0eff2269d46161607b8b02f642b
Esper0328 <esper0328@gmail.com>
2022-04-20 16:33:52 +0900

    changed base program from memo3 to memo4

--- ch9/ex03/memo/memo.go
+++ ch9/ex03/memo/memo.go
@@ -4,18 +4,14 @@
 // See page 276.
 
 // Package memo provides a concurrency-safe memoization a function of
-// type Func.  Requests for different keys run concurrently.
-// Concurrent requests for the same key result in duplicate work.
+// a function.  Requests for different keys proceed in parallel.
+// Concurrent requests for the same key block until the first completes.
+// This implementation uses a Mutex.
 package memo
 
 import "sync"
 
-type Memo struct {
-	f     Func
-	mu    sync.Mutex // guards cache
-	cache map[string]result
-}
-
+// Func is the type of the function to memoize.
 type Func func(string) (interface{}, error)
 
 type result struct {
@@ -23,26 +19,43 @@ type result struct {
 	err   error
 }
 
-func New(f Func) *Memo {
-	return &Memo{f: f, cache: make(map[string]result)}
+//!+
+type entry struct {
+	res   result
+	ready chan struct{} // closed when res is ready
 }
 
-//!+
+func New(f Func) *Memo {
+	return &Memo{f: f, cache: make(map[string]*entry)}
+}
+
+type Memo struct {
+	f     Func
+	mu    sync.Mutex // guards cache
+	cache map[string]*entry
+}
 
 func (memo *Memo) Get(key string) (value interface{}, err error) {
 	memo.mu.Lock()
-	res, ok := memo.cache[key]
-	memo.mu.Unlock()
-	if !ok {
-		res.value, res.err = memo.f(key)
-
-		// Between the two critical sections, several goroutines
-		// may race to compute f(key) and update the map.
-		memo.mu.Lock()
-		memo.cache[key] = res
+	e := memo.cache[key]
+	if e == nil {
+		// This is the first request for this key.
+		// This goroutine becomes responsible for computing
+		// the value and broadcasting the ready condition.
+		e = &entry{ready: make(chan struct{})}
+		memo.cache[key] = e
 		memo.mu.Unlock()
+
+		e.res.value, e.res.err = memo.f(key)
+
+		close(e.ready) // broadcast ready condition
+	} else {
+		// This is a repeat request for this key.
+		memo.mu.Unlock()
+
+		<-e.ready // wait for ready condition
 	}
-	return res.value, res.err
+	return e.res.value, e.res.err
 }
 
 //!-

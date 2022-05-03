6d6199a8c044694ea2d789ec9a42532321e714a7
Esper0328 <esper0328@gmail.com>
2022-05-03 15:24:38 +0900

    added Intset test for same adding behavior as map structure

---
+++ ch11/ex02/intset/intset_test.go
@@ -0,0 +1,69 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+package intset
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Example_one() {
+	//!+main
+	var x, y IntSet
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+	fmt.Println(x.String()) // "{1 9 144}"
+
+	y.Add(9)
+	y.Add(42)
+	fmt.Println(y.String()) // "{9 42}"
+
+	x.UnionWith(&y)
+	fmt.Println(x.String()) // "{1 9 42 144}"
+
+	fmt.Println(x.Has(9), x.Has(123)) // "true false"
+	//!-main
+
+	// Output:
+	// {1 9 144}
+	// {9 42}
+	// {1 9 42 144}
+	// true false
+}
+
+func Example_two() {
+	var x IntSet
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+	x.Add(42)
+
+	//!+note
+	fmt.Println(&x)         // "{1 9 42 144}"
+	fmt.Println(x.String()) // "{1 9 42 144}"
+	fmt.Println(x)          // "{[4398046511618 0 65536]}"
+	//!-note
+
+	// Output:
+	// {1 9 42 144}
+	// {1 9 42 144}
+	// {[4398046511618 0 65536]}
+}
+
+func TestAddIntSet(t *testing.T) {
+	var x IntSet
+	y := make(map[uint64]bool)
+
+	given := []int{1, 144, 9, 42}
+	for _, val := range given {
+		x.Add(val)
+		y[uint64(val)] = true
+	}
+	for _, val := range given {
+		if x.Has(val) != y[uint64(val)] {
+			t.Errorf("x.Has(%d):%t y[%d]:%t", val, x.Has(val), given, y[uint64(val)])
+		}
+	}
+}

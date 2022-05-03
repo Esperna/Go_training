91d92f5a96f7f50a3cd7db50fb1061c2f0013eaa
Esper0328 <esper0328@gmail.com>
2022-05-03 19:52:30 +0900

    added randomNotPalindome Test

--- ch11/ex03/word2/word_test.go
+++ ch11/ex03/word2/word_test.go
@@ -6,12 +6,11 @@ package word
 import (
 	"fmt"
 	"math/rand"
+	"testing"
 	"time"
 )
 
 //!+bench
-
-import "testing"
 
 //!-bench
 
@@ -96,6 +95,30 @@ func TestRandomPalindromes(t *testing.T) {
 		p := randomPalindrome(rng)
 		if !IsPalindrome(p) {
 			t.Errorf("IsPalindrome(%q) = false", p)
+		}
+	}
+}
+
+func randomNotPalindrome(rng *rand.Rand) string {
+	n := rng.Intn(25) // random length up to 24
+	runes := make([]rune, n)
+	for i := 0; i < (n+1)/2; i++ {
+		r := rune(rng.Intn(0x1000)) // random rune up to '\u0999'
+		runes[i] = r
+		runes[n-1-i] = r
+	}
+	return "a" + string(runes) + "b"
+}
+
+func TestRandomNotPrindrome(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+
+	for i := 0; i < 1000; i++ {
+		p := randomNotPalindrome(rng)
+		if IsPalindrome(p) {
+			t.Errorf("IsPalindrome(%q) = true", p)
 		}
 	}
 }

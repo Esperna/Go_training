51300c0261a7a525228f89ad38267e4be3942d29
Esper0328 <esper0328@gmail.com>
2022-05-04 15:53:21 +0900

    copied from ex11.2

---
+++ ch11/ex07/intset/mapintset.go
@@ -0,0 +1,91 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 165.
+
+// Package intset provides a set of integers based on a bit vector.
+package intset
+
+import (
+	"bytes"
+	"fmt"
+	"sort"
+)
+
+type Uint64Slice []uint64
+
+func (s Uint64Slice) Len() int           { return len(s) }
+func (s Uint64Slice) Less(i, j int) bool { return s[i] < s[j] }
+func (s Uint64Slice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+
+type MapIntSet struct {
+	words map[uint64]bool
+}
+
+func (s *MapIntSet) Has(x int) bool {
+	return s.words[uint64(x)]
+}
+
+func (s *MapIntSet) Add(x int) {
+	s.words[uint64(x)] = true
+}
+
+func (s *MapIntSet) UnionWith(t *MapIntSet) {
+	for key := range t.words {
+		s.words[key] = true
+	}
+}
+
+func (s *MapIntSet) String() string {
+	var values Uint64Slice
+	for value := range s.words {
+		values = append(values, value)
+	}
+	sort.Sort(values)
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, value := range values {
+		fmt.Fprintf(&buf, "%d", value)
+		if i < len(values)-1 {
+			buf.WriteByte(' ')
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+func (s *MapIntSet) Len() int {
+	return len(s.words)
+}
+
+func (s *MapIntSet) Remove(x int) {
+	delete(s.words, uint64(x))
+}
+
+func (s *MapIntSet) IntersectWith(t *MapIntSet) {
+	for key := range s.words {
+		if !t.words[key] {
+			delete(s.words, key)
+		}
+	}
+}
+
+func (s *MapIntSet) DifferenceWith(t *MapIntSet) {
+	for key := range s.words {
+		if t.words[key] {
+			delete(s.words, key)
+		}
+	}
+}
+
+func (s *MapIntSet) SymmetricDifference(t *MapIntSet) {
+	for key := range s.words {
+		if t.words[key] {
+			delete(s.words, key)
+			delete(t.words, key)
+		}
+	}
+	for key := range t.words {
+		s.words[key] = true
+	}
+}
---
+++ ch11/ex07/intset/mapintset_test.go
@@ -0,0 +1,186 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+package intset
+
+import (
+	"testing"
+)
+
+func TestAddMapIntSet(t *testing.T) {
+	var x1 IntSet
+	var x2 MapIntSet
+
+	x1.Add(1)
+	x1.Add(144)
+	x1.Add(9)
+
+	x2.words = make(map[uint64]bool)
+	x2.Add(1)
+	x2.Add(144)
+	x2.Add(9)
+
+	if x1.String() != x2.String() {
+		t.Errorf("x1:%s, x2:%s", x1.String(), x2.String())
+	}
+}
+
+func TestUnionWithMapIntSet(t *testing.T) {
+	var x1, y1 IntSet
+	var x2, y2 MapIntSet
+
+	x1.Add(1)
+	x1.Add(144)
+	x1.Add(9)
+	y1.Add(9)
+	y1.Add(42)
+	x1.UnionWith(&y1)
+
+	x2.words = make(map[uint64]bool)
+	y2.words = make(map[uint64]bool)
+	x2.Add(1)
+	x2.Add(144)
+	x2.Add(9)
+	y2.Add(9)
+	y2.Add(42)
+	x2.UnionWith(&y2)
+
+	if x1.String() != x2.String() {
+		t.Errorf("Want:x1==x2 Actual: x1:%s, x2:%s", x1.String(), x2.String())
+	}
+}
+
+func TestHasMapIntSet(t *testing.T) {
+	var x1 IntSet
+	var x2 MapIntSet
+
+	x1.Add(1)
+	x1.Add(144)
+	x1.Add(9)
+
+	x2.words = make(map[uint64]bool)
+	x2.Add(1)
+	x2.Add(144)
+	x2.Add(9)
+
+	for key := range x2.words {
+		if x1.Has(int(key)) != x2.Has(int(key)) {
+			t.Errorf("Want:x1.Has(key)==x2.Has(key) Actual: x1.Has(key)=%t, x2.Has(key)=%t, key=%d", x1.Has(int(key)), x2.Has(int(key)), key)
+		}
+	}
+}
+
+func TestLenMapIntSet(t *testing.T) {
+	var x1 IntSet
+	var x2 MapIntSet
+	x1.Add(1)
+	x1.Add(144)
+	x1.Add(9)
+	x1.Add(42)
+
+	x2.words = make(map[uint64]bool)
+	x2.Add(1)
+	x2.Add(144)
+	x2.Add(9)
+	x2.Add(42)
+
+	if x1.Len() != x2.Len() {
+		t.Errorf("Expected x1.Len() == x2.Len(), x1.Len():%d x2.Len():%d", x1.Len(), x2.Len())
+	}
+}
+
+func TestRemoveMapIntSet(t *testing.T) {
+	var x1 IntSet
+	var x2 MapIntSet
+	x1.Add(1)
+	x1.Add(144)
+	x1.Add(9)
+	x1.Add(42)
+	x1.Remove(9)
+
+	x2.words = make(map[uint64]bool)
+	x2.Add(1)
+	x2.Add(144)
+	x2.Add(9)
+	x2.Add(42)
+	x2.Remove(9)
+
+	if x1.String() != x2.String() {
+		t.Errorf("Expected x1.String() == x2.String(), x1.String():%s x2.String():%s", x1.String(), x2.String())
+	}
+	x1.Remove(9999)
+	x2.Remove(9999)
+
+	if x1.String() != x2.String() {
+		t.Errorf("Expected x1.String() == x2.String(), x1.String():%s x2.String():%s", x1.String(), x2.String())
+	}
+}
+
+func TestIntersectWithMapIntSet(t *testing.T) {
+	var x1, y1 IntSet
+	x1.Add(1)
+	x1.Add(144)
+	x1.Add(9)
+	y1.Add(9)
+	y1.Add(42)
+	x1.IntersectWith(&y1)
+
+	var x2, y2 MapIntSet
+	x2.words = make(map[uint64]bool)
+	y2.words = make(map[uint64]bool)
+	x2.Add(1)
+	x2.Add(144)
+	x2.Add(9)
+	y2.Add(9)
+	y2.Add(42)
+	x2.IntersectWith(&y2)
+
+	if x1.String() != x2.String() {
+		t.Errorf("Expected x1.String() == x2.String(). x1.String():%s, x2.String():%s", x1.String(), x2.String())
+	}
+}
+
+func TestDifferenceWithMapIntset(t *testing.T) {
+	var x1, y1 IntSet
+	x1.Add(1)
+	x1.Add(144)
+	x1.Add(9)
+	y1.Add(9)
+	y1.Add(42)
+	x1.DifferenceWith(&y1)
+
+	var x2, y2 MapIntSet
+	x2.words = make(map[uint64]bool)
+	y2.words = make(map[uint64]bool)
+	x2.Add(1)
+	x2.Add(144)
+	x2.Add(9)
+	y2.Add(9)
+	y2.Add(42)
+	x2.DifferenceWith(&y2)
+	if x1.String() != x2.String() {
+		t.Errorf("Expected x1.String() == x2.String(). x1.String():%s, x2.String():%s", x1.String(), x2.String())
+	}
+}
+func TestSymmetricDifferenceMapIntset(t *testing.T) {
+	var x1, y1 IntSet
+	x1.Add(1)
+	x1.Add(144)
+	x1.Add(9)
+	y1.Add(9)
+	y1.Add(42)
+	x1.SymmetricDifference(&y1)
+
+	var x2, y2 MapIntSet
+	x2.words = make(map[uint64]bool)
+	y2.words = make(map[uint64]bool)
+	x2.Add(1)
+	x2.Add(144)
+	x2.Add(9)
+	y2.Add(9)
+	y2.Add(42)
+	x2.SymmetricDifference(&y2)
+	if x1.String() != x2.String() {
+		t.Errorf("Expected x1.String() == x2.String(). x1.String():%s, x2.String():%s", x1.String(), x2.String())
+	}
+}

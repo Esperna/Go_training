30418042d718d581bd6e02da0b92ae08de1a2c0d
Esper0328 <esper0328@gmail.com>
2022-05-29 17:58:45 +0900

    implemented reading zip

--- ch10/ex02/main.go
+++ ch10/ex02/main.go
@@ -1,7 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	filepaths := []string{"archive1.zip", "archive2.zip"}
+	for _, path := range filepaths {
+		err := checkArchiveType(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = readArchive(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func checkArchiveType(filepath string) error {
+	f, err := os.Open(filepath)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Archive File type:")
+	if isZip(f) {
+		fmt.Println("zip")
+	} else if isTar(f) {
+		fmt.Println("tar")
+	} else {
+		fmt.Println("unknown")
+	}
+	f.Close()
+	return nil
+}
+
+func readArchive(filepath string) error {
+	r, err := zip.OpenReader(filepath)
+	defer r.Close()
+	for i, f := range r.File {
+		fmt.Printf("---Entry%d---\n", i+1)
+		if f.FileInfo().IsDir() {
+			fmt.Printf("name:%s\n", f.Name)
+		} else {
+			fmt.Printf("name:%s\n", f.Name)
+			fmt.Println("Contents:")
+			rc, err := f.Open()
+			if err != nil {
+				log.Fatal(err)
+			}
+			_, err = io.CopyN(os.Stdout, rc, 68)
+			if err != nil {
+				fmt.Println(err)
+			}
+			rc.Close()
+		}
+	}
+	return err
+}
+
+func isZip(r io.Reader) bool {
+	var b [2]byte
+	n, err := r.Read(b[:])
+	if err != nil {
+		log.Printf("%s", err)
+		return false
+	}
+	if n > 0 {
+		if string(b[0]) == "P" && string(b[1]) == "K" {
+			return true
+		}
+	}
+	return false
+}
+
+func isTar(r io.Reader) bool {
+	return false
 }

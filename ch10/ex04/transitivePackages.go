c1202e923c414e3f6d99911f8c7e6e7f0b758fe5
Esper0328 <esper0328@gmail.com>
2022-05-30 13:01:30 +0900

    implemented transitive packages report but not sure whether report result is correct or not

--- ch10/ex04/transitivePackages.go
+++ ch10/ex04/transitivePackages.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -17,11 +18,33 @@ func main() {
 		log.Printf("invalid num of args. Expected is 1")
 		return
 	}
-	cmd := exec.Command("go", "list", os.Args[1])
-	out, err := cmd.Output()
-	if err != nil {
-		log.Printf("cmd failed:%s", err)
-	}
-	fmt.Printf("%s", out)
+	seen := make(map[string]bool)
+	var checkDependencies func(string) TransitivePackagesResult
 
+	checkDependencies = func(arg string) TransitivePackagesResult {
+		cmd := exec.Command("go", "list", "-json", arg)
+		out, err := cmd.Output()
+		if err != nil {
+			log.Printf("cmd failed:%s", err)
+		}
+		var result TransitivePackagesResult
+		if err := json.Unmarshal(out, &result); err != nil {
+			log.Fatalf("JSON unmarshaling failed:%s", err)
+		}
+		return result
+	}
+
+	result1 := checkDependencies(os.Args[1])
+	for _, dep := range result1.Deps {
+		seen[dep] = true
+	}
+	for _, arg := range result1.Deps {
+		result2 := checkDependencies(arg)
+		for _, dep := range result2.Deps {
+			seen[dep] = true
+		}
+	}
+	for k := range seen {
+		fmt.Println(k)
+	}
 }

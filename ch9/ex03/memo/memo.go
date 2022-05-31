5e86c391ce94ae7fdc9a15e39af332de3f7a923d
Esper0328 <esper0328@gmail.com>
2022-05-31 16:12:04 +0900

    copied memo.go and memo_test.go from gopl.io/ch9/memo5

--- ch9/ex03/memo/memo.go
+++ ch9/ex03/memo/memo.go
@@ -1,70 +1,88 @@
 // Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 
-// See page 276.
+// See page 278.
 
-// Package memo provides a concurrency-safe memoization a function of
-// a function.  Requests for different keys proceed in parallel.
+// Package memo provides a concurrency-safe non-blocking memoization
+// of a function.  Requests for different keys proceed in parallel.
 // Concurrent requests for the same key block until the first completes.
-// This implementation uses a Mutex.
+// This implementation uses a monitor goroutine.
 package memo
 
-import (
-	"sync"
-	"time"
-)
+//!+Func
 
 // Func is the type of the function to memoize.
-type Func func(string, <-chan struct{}) (interface{}, error)
+type Func func(key string) (interface{}, error)
 
+// A result is the result of calling a Func.
 type result struct {
 	value interface{}
 	err   error
 }
 
-//!+
 type entry struct {
 	res   result
 	ready chan struct{} // closed when res is ready
 }
 
+//!-Func
+
+//!+get
+
+// A request is a message requesting that the Func be applied to key.
+type request struct {
+	key      string
+	response chan<- result // the client wants a single result
+}
+
+type Memo struct{ requests chan request }
+
+// New returns a memoization of f.  Clients must subsequently call Close.
 func New(f Func) *Memo {
-	return &Memo{f: f, cache: make(map[string]*entry)}
+	memo := &Memo{requests: make(chan request)}
+	go memo.server(f)
+	return memo
 }
 
-type Memo struct {
-	f     Func
-	mu    sync.Mutex // guards cache
-	cache map[string]*entry
+func (memo *Memo) Get(key string) (interface{}, error) {
+	response := make(chan result)
+	memo.requests <- request{key, response}
+	res := <-response
+	return res.value, res.err
 }
 
-func (memo *Memo) Get(key string) (value interface{}, err error) {
-	memo.mu.Lock()
-	e := memo.cache[key]
-	if e == nil {
-		// This is the first request for this key.
-		// This goroutine becomes responsible for computing
-		// the value and broadcasting the ready condition.
-		e = &entry{ready: make(chan struct{})}
-		memo.cache[key] = e
-		memo.mu.Unlock()
-		done := make(chan struct{})
-		go func() {
-			ticker := time.NewTicker(500 * time.Millisecond)
-			<-ticker.C
-			ticker.Stop()
-			close(done)
-		}()
-		e.res.value, e.res.err = memo.f(key, done)
+func (memo *Memo) Close() { close(memo.requests) }
 
-		close(e.ready) // broadcast ready condition
-	} else {
-		// This is a repeat request for this key.
-		memo.mu.Unlock()
+//!-get
 
-		<-e.ready // wait for ready condition
+//!+monitor
+
+func (memo *Memo) server(f Func) {
+	cache := make(map[string]*entry)
+	for req := range memo.requests {
+		e := cache[req.key]
+		if e == nil {
+			// This is the first request for this key.
+			e = &entry{ready: make(chan struct{})}
+			cache[req.key] = e
+			go e.call(f, req.key) // call f(key)
+		}
+		go e.deliver(req.response)
 	}
-	return e.res.value, e.res.err
 }
 
-//!-
+func (e *entry) call(f Func, key string) {
+	// Evaluate the function.
+	e.res.value, e.res.err = f(key)
+	// Broadcast the ready condition.
+	close(e.ready)
+}
+
+func (e *entry) deliver(response chan<- result) {
+	// Wait for the ready condition.
+	<-e.ready
+	// Send the result to the client.
+	response <- e.res
+}
+
+//!-monitor
--- ch9/ex03/memo/memo_test.go
+++ ch9/ex03/memo/memo_test.go
@@ -6,18 +6,20 @@ package memo_test
 import (
 	"testing"
 
-	"ch9/ex03/memo"
-	"ch9/ex03/memotest"
+	"gopl.io/ch9/memo5"
+	"gopl.io/ch9/memotest"
 )
 
 var httpGetBody = memotest.HTTPGetBody
 
 func Test(t *testing.T) {
 	m := memo.New(httpGetBody)
+	defer m.Close()
 	memotest.Sequential(t, m)
 }
 
 func TestConcurrent(t *testing.T) {
 	m := memo.New(httpGetBody)
+	defer m.Close()
 	memotest.Concurrent(t, m)
 }

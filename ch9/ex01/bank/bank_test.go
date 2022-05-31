e38edd95c7ba6bdce940c0ad5fa39317fcf97610
Esper0328 <esper0328@gmail.com>
2022-05-31 11:53:51 +0900

    added concurrent test for Deposit and Withdraw

--- ch9/ex01/bank/bank_test.go
+++ ch9/ex01/bank/bank_test.go
@@ -6,6 +6,7 @@ package bank_test
 import (
 	"ch9/ex01/bank"
 	"fmt"
+	"sync"
 	"testing"
 )
 
@@ -59,6 +60,81 @@ func TestBank(t *testing.T) {
 		}
 		if got, want := bank.Balance(), test.expected.balance; got != want {
 			t.Errorf("Balance = %d, want %d", got, want)
+		}
+	}
+	//tear down
+	bank.Withdraw(bank.Balance())
+	if bank.Balance() != 0 {
+		t.Errorf("Balance = %d, want 0", bank.Balance())
+	}
+}
+
+func TestBankConcurrentWithdraw(t *testing.T) {
+	result := make(chan bool)
+	const numOfTest = 10000
+	for i := 0; i < numOfTest; i++ {
+		ready := make(chan struct{})
+		bank.Withdraw(bank.Balance())
+		if bank.Balance() != 0 {
+			t.Errorf("Balance = %d, want 0", bank.Balance())
+		}
+		bank.Deposit(100)
+		const numOfGoRoutine = 10
+		var n sync.WaitGroup
+		for i := 0; i < numOfGoRoutine; i++ {
+			n.Add(1)
+			go func() {
+				n.Done()
+				<-ready
+				result <- bank.Withdraw(60)
+			}()
+		}
+		n.Wait()
+		close(ready)
+		successCount := 0
+		for i := 0; i < numOfGoRoutine; i++ {
+			if <-result {
+				successCount++
+			}
+		}
+		const want = 40
+		if bank.Balance() != want {
+			t.Errorf("balance is %d, want %d", bank.Balance(), want)
+		}
+		if successCount != 1 {
+			t.Errorf("deposit success count is %d, want 1", successCount)
+		}
+	}
+}
+
+func TestBankConcurrentDeposit(t *testing.T) {
+	done := make(chan struct{})
+	const numOfTest = 10000
+	for i := 0; i < numOfTest; i++ {
+		ready := make(chan struct{})
+		bank.Withdraw(bank.Balance())
+		if bank.Balance() != 0 {
+			t.Errorf("Balance = %d, want 0", bank.Balance())
+		}
+		const numOfGoRoutine = 10
+		var n sync.WaitGroup
+		for i := 0; i < numOfGoRoutine; i++ {
+			n.Add(1)
+			go func() {
+				n.Done()
+				<-ready
+				bank.Deposit(100)
+				done <- struct{}{}
+			}()
+		}
+		n.Wait()
+		close(ready)
+		for i := 0; i < numOfGoRoutine; i++ {
+			<-done
+		}
+		const want = 100 * numOfGoRoutine
+		if bank.Balance() != want {
+			t.Errorf("balance is %d, want %d", bank.Balance(), want)
 		}
 	}
 }
